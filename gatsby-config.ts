2000406142164ccc1f713c5290779b9beb82ac15
Yvonne Tang <ytang2@stanford.edu>
2023-02-17 00:30:09 -0800

    install gatsby packages and gatsby-source-storyblok; gatsby-config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,14 +2,45 @@ import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `ood-giving-campaign`,
+    title: `Stanford On Purpose`,
     siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        mergeSecurityHeaders: false,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Stanford On Purpose`,
+        start_url: `/`,
+        include_favicon: false,
+        crossOrigin: `use-credentials`,
+        icon: `src/images/icon.png`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-storyblok',
+      options: {
+        accessToken: process.env.GATSBY_PREVIEW_STORYBLOK,
+        version: process.env.NODE_ENV === 'production' ? 'published' : 'draft',
+        localAssets: true, // Optional parameter to download the images to use with Gatsby Image Plugin
+      }
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
 }
 
 export default config
