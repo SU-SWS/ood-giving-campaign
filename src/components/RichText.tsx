0a64c3df597bad550fdb3eed55e57912fb383229
Yvonne Tang <42749717+yvonnetangsu@users.noreply.github.com>
2023-03-23 19:13:09 -0700

    GIVCAMP-57 | Typescript rich text renderer (#15)
    * RichText renderer
    
    * clean up
    
    * Remove stuff I don't need for now so linter can stop complaining
    
    * Use the Typescript version of the rich text react renderer instead and update code

---
+++ src/components/RichText.tsx
@@ -0,0 +1,73 @@
+/* eslint-disable react/no-unstable-nested-components */
+import React from 'react';
+import { render, StoryblokRichtext } from 'storyblok-rich-text-react-renderer-ts';
+import { dcnb } from 'cnbuilder';
+import { Heading, Paragraph } from './Typography';
+
+export type RichTextProps = {
+  wysiwyg: StoryblokRichtext;
+  isDark?: boolean;
+  className?: string;
+  linkColor?: string;
+};
+
+export const RichText = ({
+  wysiwyg,
+  isDark,
+  className,
+}: RichTextProps) => {
+  let textColor = 'su-text-current';
+
+  if (isDark) {
+    textColor = 'su-text-black-20 print:su-text-black';
+  }
+  const rendered = render(wysiwyg, {
+    markResolvers: {
+      styled: (children, props) => React.createElement(
+        'span',
+        {
+          // eslint-disable-next-line react/destructuring-assignment
+          className: props?.class || '',
+        },
+        children,
+      ),
+      bold: (children) => <strong>{children}</strong>,
+      italic: (children) => <em>{children}</em>,
+    },
+    nodeResolvers: {
+      heading: (children, props) => {
+        const { level } = props;
+        if (level > 1 && level < 6) {
+          return (
+            <Heading as={`h${level}`} font="serif">
+              {children}
+            </Heading>
+          );
+        }
+
+        if (level === 6) {
+          return (
+            <Heading as="h6" font="serif" size="base">
+              {children}
+            </Heading>
+          );
+        }
+
+        return null;
+      },
+    },
+    blokResolvers: {
+      // TODO: Add components
+    },
+    defaultBlokResolver: (name) => (
+      <Paragraph weight="bold">
+        Missing blok resolver for blok type {name}.
+      </Paragraph>
+    ),
+    defaultStringResolver: (str) => <div>{str}</div>,
+  });
+
+  return (
+    <div className={dcnb('su-wysiwyg', textColor, className)}>{rendered}</div>
+  );
+};
