986bc9e5a4dd0981db6f87bb2cd47c3903db9ede
Yvonne Tang <ytang2@stanford.edu>
2023-04-06 15:31:28 -0700

    remove classes not needed

--- src/components/Storyblok/SbSection.tsx
+++ src/components/Storyblok/SbSection.tsx
@@ -25,7 +25,7 @@ const SbSection = ({
   blok,
 }: SbSectionProps) => (
   <Section {...storyblokEditable(blok)} key={_uid} bgColor={bgColor}>
-    {heading && <Heading as={headingLevel} size={9} uppercase font="druk" className="su-tracking-normal">{heading}</Heading>}
+    {heading && <Heading as={headingLevel} size={9} uppercase font="druk">{heading}</Heading>}
     <CreateBloks blokSection={content} />
   </Section>
 );
