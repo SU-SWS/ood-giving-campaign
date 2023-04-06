45eef8db4d30e1b8afc90dcdb9ca4a8259bdb0a2
Yvonne Tang <ytang2@stanford.edu>
2023-04-06 16:09:39 -0700

    Update storyblok Grid to use Sb prefix

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,7 @@ import { Slice } from 'gatsby';
 import { storyblokInit, apiPlugin } from 'gatsby-source-storyblok';
 import { FlexBox } from './FlexBox';
 import Teaser from './Storyblok/Teaser';
-import Grid from './Storyblok/Grid';
+import SbGrid from './Storyblok/SbGrid';
 import Feature from './Storyblok/Feature';
 import SbSection from './Storyblok/SbSection';
 
@@ -19,7 +19,7 @@ storyblokInit({
   use: [apiPlugin],
   components: {
     teaser: Teaser,
-    grid: Grid,
+    sbGrid: SbGrid,
     feature: Feature,
     sbSection: SbSection,
   },
