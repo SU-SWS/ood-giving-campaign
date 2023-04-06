0d6bd1afd70e4ccdc8a25af600d704028872f2c3
Yvonne Tang <ytang2@stanford.edu>
2023-04-05 17:19:31 -0700

    update to TW 3.3.1 which allows us to change tailwind.config to a .ts file - also updated with ts syntax

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,18 @@
+/* eslint-disable import/no-dynamic-require */
+/* eslint-disable global-require */
 /** @type {import('tailwindcss').Config} */
 
+import type { Config } from 'tailwindcss';
+
 const path = require('path');
+const decanter = require('decanter');
 
 // Path to custom Tailwind plugins for Directory
 const dir = path.resolve(__dirname, 'src/tailwind/plugins');
 
-module.exports = {
+export default {
   presets: [
-    require('decanter'),
+    decanter,
   ],
   content: [
     /**
@@ -28,4 +33,4 @@ module.exports = {
     require(`${dir}/base/gc-base.js`)(),
     require(`${dir}/components/gc-typography.js`)(),
   ],
-};
+} satisfies Config;
