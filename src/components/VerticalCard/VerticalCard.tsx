1ec42102c64a0e097a8e509879ab6ccbb251cbfc
Yvonne Tang <ytang2@stanford.edu>
2023-04-08 01:43:50 -0700

    Install framer motion and add scroll card zoom animation

--- src/components/VerticalCard/VerticalCard.tsx
+++ src/components/VerticalCard/VerticalCard.tsx
@@ -1,4 +1,5 @@
-import React, { HTMLAttributes } from 'react';
+import React, { HTMLAttributes, useRef } from 'react';
+import { motion, useInView } from 'framer-motion';
 import { dcnb } from 'cnbuilder';
 import { CtaLink } from '../Cta/CtaLink';
 import { Heading, HeadingType, Paragraph } from '../Typography';
@@ -30,49 +31,60 @@ export const VerticalCard = ({
   href,
   className,
   ...props
-}: VerticalCardProps) => (
-  <article
-    className={dcnb('su-group su-relative su-z-10', styles.textColors[textColor], className)}
-    {...props}
-  >
-    {imageSrc && (
-      <div className="su-transition-all su-aspect-w-1 su-aspect-h-1 su-rounded-none group-hover:su-rounded-tl-[10rem] group-hover:su-rounded-br-[10rem] group-focus-within:su-rounded-tl-[10rem] group-focus-within:su-rounded-br-[10rem] su-overflow-hidden">
-        <img
-          src={imageSrc}
-          alt={alt}
-          className="su-object-cover su-w-full su-h-full"
-        />
+}: VerticalCardProps) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: false });
+
+  return (
+    <article
+      style={{
+        transform: isInView ? 'none' : 'scale(0.9)',
+        opacity: isInView ? 1 : 0.6,
+        transition: 'all 1s cubic-bezier(0.17, 0.55, 0.55, 1) 0.2s',
+      }}
+      ref={ref}
+      className={dcnb('su-group su-relative su-z-10', styles.textColors[textColor], className)}
+      {...props}
+    >
+      {imageSrc && (
+        <div className="su-transition-all su-aspect-w-1 su-aspect-h-1 su-rounded-none group-hover:su-rounded-tl-[10rem] group-hover:su-rounded-br-[10rem] group-focus-within:su-rounded-tl-[10rem] group-focus-within:su-rounded-br-[10rem] su-overflow-hidden">
+          <img
+            src={imageSrc}
+            alt={alt}
+            className="su-object-cover su-w-full su-h-full"
+          />
+        </div>
+      )}
+      <div aria-hidden className={styles.tab} />
+      {heading && (
+        <Heading
+          as={headingLevel}
+          font="druk"
+          size={5}
+          leading="none"
+          className="su-mt-01em su-mb-02em"
+        >
+          {heading}
+        </Heading>
+      )}
+      <div className="su-mb-06em">
+        <CtaLink href="/about-test" className="su-z-20 su-relative" variant="chip">Taxonomy</CtaLink>
       </div>
-    )}
-    <div aria-hidden className={styles.tab} />
-    {heading && (
-      <Heading
-        as={headingLevel}
-        font="druk"
-        size={5}
-        leading="none"
-        className="su-mt-01em su-mb-02em"
-      >
-        {heading}
-      </Heading>
-    )}
-    <div className="su-mb-06em">
-      <CtaLink href="/about-test" className="su-z-20 su-relative" variant="chip">Taxonomy</CtaLink>
-    </div>
-    {body && (
-      <Paragraph size={1} leading="snug">{body}</Paragraph>
-    )}
-    {(href || link) && (
-      <CtaLink
-        icon="triangle-right"
-        animate="right"
-        sbLink={link}
-        href={href}
-        uppercase
-        className="su-stretched-link"
-      >
-        Learn How
-      </CtaLink>
-    )}
-  </article>
-);
+      {body && (
+        <Paragraph size={1} leading="snug">{body}</Paragraph>
+      )}
+      {(href || link) && (
+        <CtaLink
+          icon="triangle-right"
+          animate="right"
+          sbLink={link}
+          href={href}
+          uppercase
+          className="su-stretched-link"
+        >
+          Learn How
+        </CtaLink>
+      )}
+    </article>
+  );
+};
