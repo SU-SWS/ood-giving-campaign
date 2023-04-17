d95045a7247bc4ab7b40d5c7c5cd7a3593e28dea
Yvonne Tang <ytang2@stanford.edu>
2023-04-17 11:57:40 -0700

    Fixup WidthBox styles; add useHasMounted hook

--- src/components/WidthBox.tsx/WidthBox.styles.ts
+++ src/components/WidthBox.tsx/WidthBox.styles.ts
@@ -4,15 +4,15 @@ export const widthClasses = {
     columnStart: 'sm:su-col-start-2 md:su-col-start-3 lg:su-col-start-4 2xl:su-col-start-5',
   },
   6: {
-    column: 'md:su-col-span-10 lg:su-col-span-8 xl:su-col-span-6',
+    column: 'sm:su-col-span-12 md:su-col-span-10 lg:su-col-span-8 xl:su-col-span-6',
     columnStart: 'md:su-col-start-2 lg:su-col-start-3 xl:su-col-start-4',
   },
   8: {
-    column: 'lg:su-col-span-10 xl:su-col-span-8',
+    column: 'sm:su-col-span-12 lg:su-col-span-10 xl:su-col-span-8',
     columnStart: 'lg:su-col-start-2 xl:su-col-start-3',
   },
   10: {
-    column: 'xl:su-col-span-10',
+    column: 'sm:su-col-span-12 xl:su-col-span-10',
     columnStart: 'xl:su-col-start-2',
   },
 };
