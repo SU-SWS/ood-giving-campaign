9e56f513f5c2d6c4aaedf25444b3cf51e9be805c
Yvonne Tang <ytang2@stanford.edu>
2023-04-17 16:37:27 -0700

    Add doc

--- src/hooks/useAddUtmParams.ts
+++ src/hooks/useAddUtmParams.ts
@@ -1,5 +1,10 @@
 import { useFilteredUtmParams } from './useFilteredUtmParams';
 
+/**
+ *
+ * @param url The URL to which UTM parameters should be added.
+ * @returns A new URL string with the specified UTM parameters appended to it.
+ */
 export const useAddUtmParams = (url: string) => {
   const utmParamsToKeep = useFilteredUtmParams();
 
