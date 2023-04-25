7ac11c3aa3f5620f3868fab522d612376b75fdfc
Yvonne Tang <ytang2@stanford.edu>
2023-04-25 16:05:24 -0700

    Revert breakpoint changes

--- src/tailwind/plugins/theme/gc-screens.js
+++ src/tailwind/plugins/theme/gc-screens.js
@@ -3,12 +3,10 @@
  * 3xl: 2xl + 2 x the screen edge margins
  * 4xl: 2000px
  */
-const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = function () {
   return {
-    ...defaultTheme.screens,
-    '3xl': '1736px',
+    '3xl': '1700px',
     '4xl': '2000px',
     print: { raw: 'print' },
   };
