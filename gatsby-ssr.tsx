881a16041a837fd09db177b1c1b11b372df9bd8d
Yvonne Tang <ytang2@stanford.edu>
2023-05-17 16:22:57 -0700

    Put all the fonts together

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -21,5 +21,11 @@ export const onRenderBody: GatsbySSR['onRenderBody'] = ({
       crossOrigin="anonymous"
       key="drukwide-bold-woff2"
     />,
+    <link rel="preconnect" href="https://fonts.googleapis.com" />,
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />,
+    <link
+      href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&family=Source+Serif+Pro:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+      rel="stylesheet"
+    />,
   ]);
 };
--- src/components/PageHead/PageHead.tsx
+++ src/components/PageHead/PageHead.tsx
@@ -12,11 +12,5 @@ export const PageHead = (props: PageHeadProps) => (
     <link rel="icon" type="image/png" href="https://www-media.stanford.edu/assets/favicon/favicon-128.png" sizes="128x128" />
     <link rel="icon" type="image/png" href="https://www-media.stanford.edu/assets/favicon/favicon-32x32.png" sizes="32x32" />
     <link rel="icon" type="image/png" href="https://www-media.stanford.edu/assets/favicon/favicon-16x16.png" sizes="16x16" />
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&family=Source+Serif+Pro:ital,wght@0,400;0,700;1,400;1,700&display=swap"
-      rel="stylesheet"
-    />
   </>
 );
