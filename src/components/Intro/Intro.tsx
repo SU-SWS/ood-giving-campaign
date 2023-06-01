acb8dc029da177e86f1a5995389ca60d553f5760
Yvonne Tang <ytang2@stanford.edu>
2023-06-01 15:51:55 -0700

    spacing change

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -11,17 +11,17 @@ export const Intro = () => {
   const sectionRef = useRef<HTMLDivElement>(null);
 
   return (
-    <div ref={sectionRef} className="su-cc su-bg-saa-black su-rs-pt-10 su-rs-pb-6 su-text-white">
-      <FlexBox alignItems="center">
-        <Bracket isSolid className="su-w-100" />
-        <Paragraph font="serif" variant="overview" noMargin className="su-max-w-900 su-mx-auto">
+    <div ref={sectionRef} className="su-bg-saa-black su-rs-pt-10 su-rs-pb-6 su-text-white">
+      <FlexBox alignItems="stretch" justifyContent="center">
+        <Bracket isSolid className="su-w-[7vw] 2xl:su-w-[15rem] su-shrink-0" />
+        <Paragraph font="serif" weight="semibold" variant="intro" noMargin className="su-max-w-1000 su-rs-p-6">
           The present demands immediate action—and the future demands immediate impact.
           Looking at the challenges we face today—some of which threaten the future of
           life on Earth—we feel a sense of urgency.
           This is the time for us all to come together with purpose and intent,
           to go further, faster for the sake of the world.
         </Paragraph>
-        <Bracket isClose isSolid className="su-w-100" />
+        <Bracket isClose isSolid className="su-w-[7vw] 2xl:su-w-[15rem] su-shrink-0" />
       </FlexBox>
     </div>
   );
