bb40132a4599800515901e3348af4f8acd36ad3a
Yvonne Tang <ytang2@stanford.edu>
2023-06-07 13:02:29 -0700

    First pass adding line in Stories section

--- src/components/Temporary/ProgressStories.tsx
+++ src/components/Temporary/ProgressStories.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useScroll, useSpring, m } from 'framer-motion';
 import { Container } from '../Container';
 import { Heading, Paragraph } from '../Typography';
 import { AnimateInView } from '../Animate';
@@ -10,29 +11,40 @@ type ProgressStoriesProps = {
 };
 
 export const ProgressStories = ({ bgImage, children }: ProgressStoriesProps) => {
-  const bg = getProcessedImage(bgImage, '1900x0');
+  const bg = getProcessedImage(bgImage, '3000x0');
+  const introRef = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: introRef,
+    offset: ['-30vh', '30%'],
+  });
+  const scaleY = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
 
   return (
     <Container width="full" bgColor="black" pb={10} className="su-relative">
       <div
-        className="su-relative su-pt-[10vw] su-pb-[7vw]"
-        style={{
-          backgroundImage: `url('${bg}')`,
-          backgroundSize: 'contain',
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'center center',
-        }}
+        className="su-relative su-pb-[7vw] su-bg-black-true"
+        style={{ background: `url('${bg}') center center / contain no-repeat` }}
       >
         <div className="su-cc su-bg-transparent su-relative su-z-10">
           <AnimateInView duration={0.6} animation="slideUp">
-            <Heading font="druk" className="su-fluid-type-8 3xl:su-gc-splash xl:su-max-w-[47%] su-mb-600 2xl:su-whitespace-pre-line" leading="none">
-              Progress<br />
-              is nothing<br />
-              without purpose.
-            </Heading>
+            <div className="su-relative su-rs-pt-9" ref={introRef}>
+              <Heading font="druk" className="su-fluid-type-8 3xl:su-gc-splash xl:su-max-w-[47%] su-pb-[23vw] 2xl:su-whitespace-pre-line" leading="none">
+                Progress<br />
+                is nothing<br />
+                without purpose.
+              </Heading>
+              <m.div
+                className="su-hidden md:su-block su-w-2 su-absolute su-top-0 su-left-[50%] su--ml-1 su-h-full su-origin-top su-bg-current"
+                style={{ scaleY }}
+              />
+            </div>
           </AnimateInView>
           <AnimateInView delay={0.2} duration={0.6} animation="slideUp">
-            <div className="lg:su-w-[80%] 2xl:su-w-[60%] su-mx-auto">
+            <div className="lg:su-w-[80%] 2xl:su-w-[60%] su-mx-auto su-rs-pt-5">
               <Paragraph font="serif" weight="semibold" variant="overview" leading="snug" className="su-max-w-700 su-mx-auto">
                 Stanford, at its heart, is a community of changemakers.
                 Learn more about the people driving us toward a brighter tomorrow.
@@ -41,7 +53,7 @@ export const ProgressStories = ({ bgImage, children }: ProgressStoriesProps) => 
           </AnimateInView>
         </div>
         <div className="su-absolute su-top-0 su-h-[60vw] 2xl:su-h-[40vw] 3xl:su-h-[35vw] su-w-full su-bg-gradient-to-b su-from-gc-black su-via-[#35459A]" />
-        <div className="su-absolute su-bottom-0 su-h-400 su-w-full su-bg-gradient-to-t su-from-gc-black su-to-black-true " />
+        <div className="su-absolute su-bottom-0 su-h-[10vw] su-w-full su-bg-gradient-to-t su-from-gc-black su-to-black-true " />
       </div>
       <Container bgColor="black">{children}</Container>
     </Container>
