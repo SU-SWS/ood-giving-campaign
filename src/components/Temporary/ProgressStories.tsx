5d3422df9a4ad46cf45690c5fbf52dd8db40952c
Yvonne Tang <ytang2@stanford.edu>
2023-06-07 22:44:02 -0700

    Stories heading animation fixup

--- src/components/Temporary/ProgressStories.tsx
+++ src/components/Temporary/ProgressStories.tsx
@@ -30,19 +30,19 @@ export const ProgressStories = ({ bgImage, children }: ProgressStoriesProps) => 
         style={{ background: `url('${bg}') center center / contain no-repeat` }}
       >
         <div className="su-cc su-bg-transparent su-relative su-z-10">
-          <AnimateInView duration={0.6} animation="slideUp">
-            <div className="su-relative su-rs-pt-9" ref={introRef}>
+          <div className="su-relative su-rs-pt-9" ref={introRef}>
+            <AnimateInView duration={0.6} delay={0.2} animation="slideUp">
               <Heading font="druk" className="su-fluid-type-8 su-mb-0 3xl:su-gc-splash xl:su-max-w-[47%] su-pb-[23vw] 2xl:su-whitespace-pre-line" leading="none">
                 Progress<br />
                 is nothing<br />
                 without purpose.
               </Heading>
-              <m.div
-                className="su-hidden md:su-block su-w-2 su-absolute su-top-0 su-left-[50%] su--ml-1 su-h-full su-origin-top su-bg-current"
-                style={{ scaleY }}
-              />
-            </div>
-          </AnimateInView>
+            </AnimateInView>
+            <m.div
+              className="su-hidden md:su-block su-w-2 su-absolute su-top-0 su-left-[50%] su--ml-1 su-h-full su-origin-top su-bg-current"
+              style={{ scaleY }}
+            />
+          </div>
           <AnimateInView delay={0.2} duration={0.6} animation="slideUp">
             <div className="lg:su-w-[80%] 2xl:su-w-[60%] su-mx-auto su-rs-pt-5">
               <Paragraph font="serif" weight="semibold" variant="overview" leading="snug" className="su-max-w-700 su-mx-auto">
@@ -52,8 +52,8 @@ export const ProgressStories = ({ bgImage, children }: ProgressStoriesProps) => 
             </div>
           </AnimateInView>
         </div>
-        <div className="su-absolute su-top-0 su-h-[60vw] 2xl:su-h-[40vw] 3xl:su-h-[35vw] su-w-full su-bg-gradient-to-b su-from-gc-black su-via-[#35459A]" />
-        <div className="su-absolute su-bottom-0 su-h-[15vw] su-w-full su-bg-gradient-to-t su-from-gc-black su-to-black-true " />
+        <div className="su-absolute su-top-0 su-h-[50vw] 2xl:su-h-[40vw] 3xl:su-h-[35vw] su-w-full su-bg-gradient-to-b su-from-gc-black su-via-[#35459A]" />
+        <div className="su-absolute su-bottom-0 su-h-[20vw] 2xl:su-h-[15vw] su-w-full su-bg-gradient-to-t su-from-gc-black su-to-black-true" />
       </div>
       <Container bgColor="black">{children}</Container>
     </Container>
