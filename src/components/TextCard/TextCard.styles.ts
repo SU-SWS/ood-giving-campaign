062ad3dc731f432b3794ff65f0701be1b98336b6
Yvonne Tang <ytang2@stanford.edu>
2023-06-27 16:32:22 -0700

    TextCard initial setup

---
+++ src/components/TextCard/TextCard.styles.ts
@@ -0,0 +1,1 @@
+export const root = 'su-rs-p-4';
---
+++ src/components/TextCard/TextCard.tsx
@@ -0,0 +1,47 @@
+import React, { HTMLAttributes } from 'react';
+import { cnb } from 'cnbuilder';
+import { AnimateInView, AnimationType } from '../Animate';
+import { Text } from '../Typography';
+import { FlexBox } from '../FlexBox';
+import { bgTextColorPairsBlackWhite, BgTextColorPairBlackWhiteType } from '../../utilities/datasource';
+import * as styles from './TextCard.styles';
+
+export type TextCardProps = HTMLAttributes<HTMLDivElement> & {
+  text?: string;
+  color?: BgTextColorPairBlackWhiteType;
+  font?: 'serif' | 'druk6' | 'druk7';
+  colSpan?: 1 | 2 | 3;
+  animation?: AnimationType;
+  delay?: number;
+};
+
+export const TextCard = ({
+  text,
+  color = 'white',
+  font = 'serif',
+  colSpan = 1,
+  animation = 'none',
+  delay,
+  className,
+  ...props
+}: TextCardProps) => {
+  const drukFontSize = font === 'druk6' ? 6 : 7;
+
+  return (
+    <AnimateInView animation={animation} delay={delay}>
+      <FlexBox
+        direction="col"
+        className={cnb(styles.root, className)}
+        {...props}
+      >
+        <Text
+          font={font === 'serif' ? 'serif' : 'druk'}
+          size={font === 'serif' ? 4 : drukFontSize}
+          weight={font === 'serif' ? 'semibold' : 'black'}
+        >
+          {text}
+        </Text>
+      </FlexBox>
+    </AnimateInView>
+  );
+};
