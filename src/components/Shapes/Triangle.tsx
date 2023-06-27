db3431646924481b3d461698a7c92be9413c18be
Yvonne Tang <ytang2@stanford.edu>
2023-06-26 18:59:18 -0700

    Update Triangle component

--- src/components/Shapes/Triangle.tsx
+++ src/components/Shapes/Triangle.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { cnb } from 'cnbuilder';
 
-export const Triangle = (props: React.SVGProps<SVGSVGElement>) => (
+type TriangleProps = React.SVGProps<SVGSVGElement>;
+
+export const Triangle = ({ className, ...rest }: TriangleProps) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 60 30"
-    {...props}
+    className={cnb('su-w-30 sm:su-w-40 lg:su-w-60', className)}
+    {...rest}
   >
     <path d="M60 0H0l29.23 30L60 0z" />
   </svg>
