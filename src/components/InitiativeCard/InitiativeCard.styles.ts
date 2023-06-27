0986b0f2885644f3a753d7198689c671f485ef5c
Yvonne Tang <ytang2@stanford.edu>
2023-06-27 00:43:09 -0700

    play with hcous effects

--- src/components/InitiativeCard/InitiativeCard.styles.ts
+++ src/components/InitiativeCard/InitiativeCard.styles.ts
@@ -1,8 +1,6 @@
 import { cnb } from 'cnbuilder';
 
-export const root = 'su-relative su-w-full su-group su-h-full';
-
-export const cardWrapper = 'su-group';
+export const root = 'su-group su-relative su-w-full su-h-full';
 
 export const topWrapper = 'su-relative';
 
@@ -10,7 +8,7 @@ export const imageWrapper = 'su-transition-all su-aspect-w-1 su-aspect-h-1 sm:su
 
 export const image = 'su-object-cover su-w-full su-h-full group-hover:su-scale-105 group-focus-within:su-scale-105 su-transition-transform';
 
-export const heading = 'su-absolute su-bottom-0 su-w-full su-bg-black-true/60 su-text-white su-rs-p-1 su-mb-0 group-hover:su-bg-black-true/70 su-transition-colors su-text-shadow-sm';
+export const heading = 'su-absolute su-bottom-0 su-w-full su-bg-black-true/60 su-text-white su-rs-p-1 su-mb-0 group-hover:su-bg-black-true/70 su-transition-all su-text-shadow-sm group-hover:su-rs-py-2 group-focus-within:su-rs-py-2';
 
 export const bodyWrapper = 'su-grow su-bg-gc-black su-text-black-10 su-rs-pt-2 su-rs-pr-1';
 
@@ -18,6 +16,6 @@ export const body = (hasTabColor: boolean) => cnb('su-rs-pl-2 su-text-current', 
   'su-border-l-[1.4rem] md:su-border-l-[1.8rem]': hasTabColor,
 });
 
-export const cta = 'su-bg-gc-black su-text-white hocus:su-text-white su-stretched-link su-no-underline su-rs-py-1 su-rs-pr-1';
+export const cta = 'su-inline-block su-bg-gc-black su-text-white hocus:su-text-white su-stretched-link su-no-underline su-rs-py-1 su-rs-pr-1';
 
-export const arrowIcon = 'su-block su-mb-0 su-mt-auto su-w-34 su-ml-auto su-mr-0  group-hover:su-translate-x-03em group-focus-within:su-translate-x-03em su-will-change-transform';
+export const arrowIcon = 'su-inline-block su-mb-0 su-mt-auto su-w-34 su-ml-auto su-mr-0  group-hover:su-translate-x-03em';
