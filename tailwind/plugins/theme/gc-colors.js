b012799b5ca53642fa13ba7a56cbd2fd15227304
Yvonne Tang <ytang2@stanford.edu>
2023-07-14 09:48:58 -0700

    Remove red colors that've been added to decanter

--- tailwind/plugins/theme/gc-colors.js
+++ tailwind/plugins/theme/gc-colors.js
@@ -13,13 +13,5 @@ module.exports = function () {
     slate: '#4E4B48',
     'gc-sky': '#4287BD', // For homepage hero sky gradient
     'gc-black': '#17171A',
-    // Colors inherited from the SAA project
-    'digital-red': {
-      xlight: '#F83535', // Passed contrast test for black background
-    },
-    'cardinal-red': {
-      xdark: '#7A0000', // Passed contrast test with digital-red-xlight as text
-      xxdark: '#541107', // Used for hover/focus color for xdark
-    },
   };
 };
