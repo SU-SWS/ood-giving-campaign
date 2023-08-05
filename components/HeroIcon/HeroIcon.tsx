bdbda3d6e793efc498974dc9411e1e5aeac452db
Yvonne Tang <ytang2@stanford.edu>
2023-08-04 17:04:46 -0700

    Fix typecasting in HeroIcon

--- components/HeroIcon/HeroIcon.tsx
+++ components/HeroIcon/HeroIcon.tsx
@@ -19,7 +19,7 @@ export const HeroIcon = ({
 
   // Set default base style so icon has reasonable size if used out of the box
   // noBaseStyle boolean allows for user to not attach any base styles if needed
-  const baseStyle = noBaseStyle ? '' : styles.iconBaseStyle[icon as styles.IconBaseStyleType] || styles.iconBaseStyle.default;
+  const baseStyle = noBaseStyle ? '' : styles.iconBaseStyle[icon] || styles.iconBaseStyleDefault;
   const heroIconStyle = cnb('transition', baseStyle);
 
   return (
