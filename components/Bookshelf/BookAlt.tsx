3fb3b5516aaeea4416e7b95861395b017b8a62e9
Yvonne Tang <ytang2@stanford.edu>
2023-08-16 00:21:32 -0700

    More bookshelf state and animation

--- components/Bookshelf/BookAlt.tsx
+++ components/Bookshelf/BookAlt.tsx
@@ -2,6 +2,7 @@ import { m, AnimatePresence, useWillChange } from 'framer-motion';
 import { cnb } from 'cnbuilder';
 import { FlexBox } from '../FlexBox';
 import { Heading, Text, Paragraph } from '../Typography';
+import { ImageOverlay } from '../ImageOverlay';
 import { getProcessedImage } from '@/utilities/getProcessedImage';
 
 type BookAltProps = {
@@ -34,8 +35,8 @@ export const BookAlt = ({
         aria-expanded={isOpen}
         onClick={() => setExpanded(isOpen ? false : i)}
         className={cnb(
-          'group relative transition-all overflow-hidden bg-black-50 h-1200 shrink-0 hocus-visible:underline decoration-white rounded-[5rem]',
-          isOpen ? 'w-1200' : 'w-[calc((100%_-_144px_-_1200px)/3)]',
+          'group relative transition-all top-0 overflow-hidden bg-black-50 h-1200 shrink-0 rounded-[5rem]',
+          isOpen ? 'w-1200 !h-600 -top-600' : 'w-[calc((100%_-_144px_-_1200px)/3)]',
           expanded === false ? '!w-[calc((100%_-_144px)/4)]' : '',
           buttonClassName,
         )}
@@ -43,29 +44,28 @@ export const BookAlt = ({
         <img
           src={getProcessedImage(imgSrc, '1200x1200')}
           alt=""
-          className="w-full h-full object-cover object-left"
+          className="w-full h-full object-cover object-top"
         />
-        <div className="">
-          <Text
-            color="white"
-            font="mono"
-            size={4}
-            leading="tight"
-            weight="bold"
-            align="left"
-            className={cnb(
-              !isOpen && expanded ? '-rotate-90 w-400 left-[7vw]' : 'left-48',
-              'transition-transform origin-bottom-left absolute bottom-200 z-10',
-            )}
-          >
-            {title}
-          </Text>
-          {isOpen && (
-            <Paragraph variant="card" color="white" leading="display" className="absolute bottom-40 z-20 rs-px-3 max-w-[74rem]" align="left">
-              {body}
-            </Paragraph>
+        <div className="absolute top-0 right-0 bg-gradient-to-t from-black/70 via-50% via-transparent w-full h-full" />
+        <Text
+          color="white"
+          font="mono"
+          size={4}
+          leading="none"
+          weight="bold"
+          align="left"
+          className={cnb(
+            !isOpen && expanded ? '-rotate-180 left-[7vw] [writing-mode:vertical-rl] h-400' : 'rs-px-2',
+            'transition-transform origin-top-left relative z-10 bottom-300 group-hocus:no-underline',
           )}
-        </div>
+        >
+          {title}
+        </Text>
+        {isOpen && (
+          <Paragraph variant="card" color="white" leading="display" className="absolute bottom-60 z-20 rs-px-3 max-w-[74rem]" align="left">
+            {body}
+          </Paragraph>
+        )}
       </button>
     </>
   );
