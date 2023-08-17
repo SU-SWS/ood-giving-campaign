9b035e219974c2cf45984195ee590710e833993c
Yvonne Tang <ytang2@stanford.edu>
2023-08-16 17:36:21 -0700

    AnimatedText effect

--- components/StoryPoC/ProgressStory.tsx
+++ components/StoryPoC/ProgressStory.tsx
@@ -1,9 +1,9 @@
 import { useRef } from 'react';
 import { useScroll, m, useTransform } from 'framer-motion';
 import { AnimateInView } from '../Animate';
+import { AnimatedText } from '../AnimatedText/AnimatedText';
 import { Container } from '../Container';
 import { Grid } from '../Grid';
-import { ImageOverlay } from '../ImageOverlay';
 import { BookshelfAlt } from '../Bookshelf/BookshelfAlt';
 import { Heading, Paragraph, Text } from '../Typography';
 import { getProcessedImage } from '@/utilities/getProcessedImage';
@@ -19,13 +19,13 @@ export const ProgressStory = () => {
           className="absolute w-full h-full object-cover object-top top-0 left-0"
         />
         <Grid lg={12} className="relative" gap="default">
-          <AnimateInView animation="slideUp" className="relative z-10 max-w-1200 2xl:col-start-2 col-span-11">
-            <Heading as="h1" font="mono" size="f8" leading="tight" className="rs-mb-1 max-w-1200">
-              Progress is nothing without purpose
+          <AnimateInView animation="slideUp" className="relative z-10 max-w-1200 col-span-12 2xl:col-start-2 2xl:col-span-8">
+            <Heading as="h1" font="mono" size="f7" leading="tight" className="rs-mb-1">
+              <AnimatedText text="Progress is nothing without purpose" />
             </Heading>
             <Text color="white">by Sarah Jane Staats</Text>
             <Text color="white">May 5, 2023</Text>
-            <Paragraph font="serif" weight="semibold" leading="display" size={2} noMargin className="mt-[30vw] max-w-1200">
+            <Paragraph font="serif" weight="semibold" leading="display" size={2} noMargin className="mt-[35vw] max-w-1200">
               Stanford, at its heart, is a community of changemakers. Where there is doubt, we make discoveries.
               Where there are fundamental questions, we make discoveries. Where there is need, we make a difference.
               Our aim is to leave the world better than we found it.
