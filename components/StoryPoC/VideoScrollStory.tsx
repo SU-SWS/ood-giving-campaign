c039eddd768f2d35a72d8b4b2b0f4b8ad3e12cdd
Yvonne Tang <ytang2@stanford.edu>
2023-08-23 15:55:37 -0700

    Pivoting to test blend mode

--- components/StoryPoC/VideoScrollStory.tsx
+++ components/StoryPoC/VideoScrollStory.tsx
@@ -1,30 +1,26 @@
 'use client';
-
 import { useRef } from 'react';
-import {
-  useScroll, useMotionValueEvent,
-} from 'framer-motion';
+import { useScroll, m, useTransform } from 'framer-motion';
+import { scroll } from 'framer-motion/dom';
 import { AnimateInView } from '../Animate';
 import { Container } from '../Container';
 import { Grid } from '../Grid';
 import { Heading, Paragraph, Text } from '../Typography';
+import { getProcessedImage } from '@/utilities/getProcessedImage';
 import { getMaskedAsset } from '@/utilities/getMaskedAsset';
 
 export const VideoScrollStory = () => {
-  const contentRef = useRef<HTMLDivElement>(null);
-  const videoRef = useRef<HTMLVideoElement>(null);
-  const { scrollYProgress } = useScroll({
-    target: contentRef,
-    offset: ['-100vh', 'end end'],
-  });
-  const video = videoRef.current;
-  // Tie scrollYProgress to the video time
-  useMotionValueEvent(scrollYProgress, 'change', (latest) => {
-    if (video && video?.readyState) {
-      video.pause();
-      video.currentTime = latest * video?.duration;
-    }
-  });
+  // const contentRef = useRef<HTMLDivElement>(null);
+  // const videoRef = useRef<HTMLVideoElement>(null);
+  // const video = videoRef.current;
+  // video?.pause();
+
+  // // Scrub through the video on scroll
+  // scroll((progress) => {
+  //   if (video?.readyState) {
+  //     video.currentTime = video?.duration * progress;
+  //   }
+  // });
 
   return (
     <Container width="full" className="relative" bgColor="black">
@@ -37,22 +33,33 @@ export const VideoScrollStory = () => {
           alt="Something alt"
         /> */}
         <video
-          ref={videoRef}
+          // ref={videoRef}
+          playsInline
+          autoPlay
           preload="auto"
           muted
-          playsInline
           loop
+          aria-label="Background Video"
           className="block w-full h-full object-cover"
         >
-          <source
-            src={getMaskedAsset('https://a-us.storyblok.com/f/1005200/x/fb44369daa/output_960.mp4')}
-            type="video/mp4"
-          />
+          <source src={getMaskedAsset('https://a-us.storyblok.com/f/1005200/x/80ddd8341f/starloop.webm')} type="video/webm" />
+          <source src={getMaskedAsset('https://a-us.storyblok.com/f/1005200/x/1a881ffc0a/starloop.mp4')} type="video/mp4" />
           <p>Your browser does not support HTML video.</p>
         </video>
+        {/* <video
+          // ref={videoRef}
+          autoPlay
+          muted
+          playsInline
+          preload="auto"
+          loop
+          src={getMaskedAsset('https://a-us.storyblok.com/f/1005200/x/80ddd8341f/starloop.webm')}
+          className="block w-full h-full object-cover"
+        /> */}
         <div className="absolute top-0 r-0 w-full h-full bg-robins-egg mix-blend-difference" />
+        <div className="absolute top-0 r-0 w-full h-full bg-black mix-blend-difference" />
       </div>
-      <div ref={contentRef} className="relative w-2/3 lg:w-1/3 mx-auto text-white z-10 rs-py-10">
+      <div className="relative w-2/3 lg:w-1/3 mx-auto text-white z-10 rs-py-10">
         <section>
           <div className="w-[200%] -ml-[50%] mb-500">
             <Heading
@@ -77,8 +84,6 @@ export const VideoScrollStory = () => {
           <Paragraph font="serif" className="text-white font-semibold text-30 mt-500">
             Cras dui ipsum, aliquet eget nibh ut, sollicitudin pharetra risus.
           </Paragraph>
-        </section>
-        <section>
           <Paragraph font="serif" className="text-white font-semibold text-30 mt-500">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget pulvinar ipsum, ut sodales odio.
             Cras dui ipsum, aliquet eget nibh ut, sollicitudin pharetra risus.
