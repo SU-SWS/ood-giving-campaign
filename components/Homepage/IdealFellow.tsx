fea586a8bf949672b15877c2c94217fc55cdff35
Yvonne Tang <ytang2@stanford.edu>
2023-09-19 19:16:27 -0700

    need to shift image up a bit to avoid gap due to parallax motion

--- components/Homepage/IdealFellow.tsx
+++ components/Homepage/IdealFellow.tsx
@@ -1,7 +1,6 @@
 import { AnimateInView } from '../Animate';
 import { Container } from '../Container';
-import { Grid } from '../Grid';
-import { Heading, Paragraph, Text } from '../Typography';
+import { Heading, Text } from '../Typography';
 import { getProcessedImage } from '@/utilities/getProcessedImage';
 import { Parallax } from '../Parallax/Parallax';
 
@@ -9,10 +8,10 @@ export const IdealFellow = () => {
   const bgImage = getProcessedImage('https://a-us.storyblok.com/f/1005200/2560x1707/c4fba877e8/firefly-20230919145441.png', '2000x1600');
   return (
     <Container width="full" bgColor="white" className="h-800 lg:h-[150rem] overflow-hidden relative">
-      <Parallax offset={90}>
+      <Parallax offset={100}>
         <div
           style={{ backgroundImage: `url('${bgImage}'` }}
-          className="-mt-80 h-[160rem] flex items-center justify-center bg-center bg-cover bg-no-repeat"
+          className="-mt-100 h-[160rem] flex items-center justify-center bg-center bg-cover bg-no-repeat"
         />
       </Parallax>
       <div className="max-w-full absolute bottom-80 left-0 3xl:left-[20vw]">
@@ -22,9 +21,9 @@ export const IdealFellow = () => {
         />
       </div>
       <Heading size="base" color="white" className="absolute bottom-300 ml-[20vw]">
-        <Text font="druk" size="f8" leading="display">Shape</Text>
-        <Text font="serif" weight="semibold" italic size="f9" className="leading-[0.9]">what’s</Text>
-        <Text font="serif" weight="semibold" italic size="f9" className="leading-[0.9]">next</Text>
+        <Text as="span" font="druk" size="f8" leading="display" className="block">Shape</Text>
+        <Text as="span" font="serif" weight="semibold" italic size="f9" className="leading-[0.9] block">what’s</Text>
+        <Text as="span" font="serif" weight="semibold" italic size="f9" className="leading-[0.9] block">next</Text>
       </Heading>
       <div className="absolute bottom-300 right-0 bg-gc-black w-5/12 rs-px-4 rs-pt-5 rs-pb-1">
         <Text variant="caption" color="white" className="mb-04em">Preparing citizens</Text>
