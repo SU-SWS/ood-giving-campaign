b0b40d0c0956b67d68ebdcbea77dcfdfab90ca35
Yvonne Tang <ytang2@stanford.edu>
2023-09-19 18:52:54 -0700

    Ideal fellow section

--- components/Homepage/IdealFellow.tsx
+++ components/Homepage/IdealFellow.tsx
@@ -8,18 +8,38 @@ import { Parallax } from '../Parallax/Parallax';
 export const IdealFellow = () => {
   const bgImage = getProcessedImage('https://a-us.storyblok.com/f/1005200/2560x1707/c4fba877e8/firefly-20230919145441.png', '2000x1500');
   return (
-    <Container width="full" bgColor="white" className="h-400 lg:h-[130rem] overflow-hidden relative">
+    <Container width="full" bgColor="white" className="h-800 lg:h-[140rem] overflow-hidden relative">
       <Parallax offset={90}>
         <div
           style={{ backgroundImage: `url('${bgImage}'` }}
-          className="-mt-100 h-[140rem] flex items-center justify-center bg-center bg-cover bg-no-repeat"
+          className="-mt-80 h-[150rem] flex items-center justify-center bg-bottom-right bg-cover bg-no-repeat"
         />
       </Parallax>
-      <div className="-mt-100 h-[150rem] absolute bottom-100 right-[-10vw]">
+      <div className="max-w-full absolute bottom-80 left-0 3xl:left-[7vw]">
         <img
-          src={getProcessedImage('https://a-us.storyblok.com/f/1005200/2560x1707/9f15c46120/hector-transparent.png', '2000x1500')}
+          src={getProcessedImage('https://a-us.storyblok.com/f/1005200/1486x1198/9fe327ec46/hector-transparent.png', '1450x0')}
           alt=""
         />
+      </div>
+      <Heading size="base" color="white" className="absolute bottom-300 ml-[20vw] [text-shadow:#c89b59a3_0_0_16px]">
+        <Text font="druk" size="f8" leading="display">Shape</Text>
+        <Text font="serif" weight="semibold" italic size="f9" className="leading-[0.9]">what’s</Text>
+        <Text font="serif" weight="semibold" italic size="f9" className="leading-[0.9]">next</Text>
+      </Heading>
+      <div className="absolute bottom-300 right-0 bg-black w-5/12 rs-px-4 rs-pt-5 rs-pb-1">
+        <Text variant="caption" color="white" className="mb-04em">Preparing citizens</Text>
+        <Heading color="white" weight="normal">
+          2022-23 IDEAL fellow Hector Callejas untangles racial inequality
+        </Heading>
+        <Text variant="card" color="white">
+          by Sarah Jane Staats
+        </Text>
+        <Text variant="card" color="white">
+          May 5, 2023
+        </Text>
+        <Text variant="caption" color="black-40" className="rs-mt-3 -mr-30" align="right">
+          Photo credit lorem ipsum dolar sit amet vestibulum perimentium.
+        </Text>
       </div>
     </Container>
   );
