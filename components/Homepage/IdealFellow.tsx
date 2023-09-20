10be7aff721316c34713c34c6dc2e14a92241913
Yvonne Tang <ytang2@stanford.edu>
2023-09-19 19:12:29 -0700

    maybe this

--- components/Homepage/IdealFellow.tsx
+++ components/Homepage/IdealFellow.tsx
@@ -6,27 +6,27 @@ import { getProcessedImage } from '@/utilities/getProcessedImage';
 import { Parallax } from '../Parallax/Parallax';
 
 export const IdealFellow = () => {
-  const bgImage = getProcessedImage('https://a-us.storyblok.com/f/1005200/2560x1707/c4fba877e8/firefly-20230919145441.png', '2000x1500');
+  const bgImage = getProcessedImage('https://a-us.storyblok.com/f/1005200/2560x1707/c4fba877e8/firefly-20230919145441.png', '2000x1600');
   return (
-    <Container width="full" bgColor="white" className="h-800 lg:h-[140rem] overflow-hidden relative">
+    <Container width="full" bgColor="white" className="h-800 lg:h-[150rem] overflow-hidden relative">
       <Parallax offset={90}>
         <div
           style={{ backgroundImage: `url('${bgImage}'` }}
-          className="-mt-80 h-[150rem] flex items-center justify-center bg-bottom-right bg-cover bg-no-repeat"
+          className="-mt-80 h-[160rem] flex items-center justify-center bg-center bg-cover bg-no-repeat"
         />
       </Parallax>
-      <div className="max-w-full absolute bottom-80 left-0 3xl:left-[7vw]">
+      <div className="max-w-full absolute bottom-80 left-0 3xl:left-[20vw]">
         <img
-          src={getProcessedImage('https://a-us.storyblok.com/f/1005200/1486x1198/9fe327ec46/hector-transparent.png', '1450x0')}
+          src={getProcessedImage('https://a-us.storyblok.com/f/1005200/1615x1306/ef8fffb741/hector-hand-full-transparent-cropped.png', '1500x0')}
           alt=""
         />
       </div>
-      <Heading size="base" color="white" className="absolute bottom-300 ml-[20vw] [text-shadow:#c89b59a3_0_0_16px]">
+      <Heading size="base" color="white" className="absolute bottom-300 ml-[20vw]">
         <Text font="druk" size="f8" leading="display">Shape</Text>
         <Text font="serif" weight="semibold" italic size="f9" className="leading-[0.9]">what’s</Text>
         <Text font="serif" weight="semibold" italic size="f9" className="leading-[0.9]">next</Text>
       </Heading>
-      <div className="absolute bottom-300 right-0 bg-black w-5/12 rs-px-4 rs-pt-5 rs-pb-1">
+      <div className="absolute bottom-300 right-0 bg-gc-black w-5/12 rs-px-4 rs-pt-5 rs-pb-1">
         <Text variant="caption" color="white" className="mb-04em">Preparing citizens</Text>
         <Heading color="white" weight="normal">
           2022-23 IDEAL fellow Hector Callejas untangles racial inequality
