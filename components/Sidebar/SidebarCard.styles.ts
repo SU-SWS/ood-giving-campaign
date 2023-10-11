5ffd55b74cb83dc59c50a6a3b81377c853dd6aff
Yvonne Tang <ytang2@stanford.edu>
2023-10-11 14:20:45 -0700

    finetune

--- components/Sidebar/SidebarCard.styles.ts
+++ components/Sidebar/SidebarCard.styles.ts
@@ -1,6 +1,6 @@
 import { cnb } from 'cnbuilder';
 
-export const root = 'relative overflow-hidden';
+export const root = 'relative overflow-hidden @container';
 
 export const content = (
   hasBgColor?: boolean,
@@ -9,7 +9,7 @@ export const content = (
 ) => cnb('', {
   'rs-py-1': hasBgColor,
   'rs-py-0': !hasBgColor,
-  'rs-pr-1 rs-pl-3 border-r-[1.4rem] xl:border-r-[2rem]': hasBarColor && barOnRight,
-  'rs-pl-1 rs-pr-3 border-l-[1.4rem] xl:border-l-[2rem]': hasBarColor && !barOnRight,
+  'rs-pr-2 rs-pl-3 border-r-[1.4rem] xl:border-r-[2rem] @2xl:pr-75 @2xl:pl-95': hasBarColor && barOnRight,
+  'rs-pl-2 rs-pr-3 border-l-[1.4rem] xl:border-l-[2rem] @2xl:pl-75 @2xl:pr-95': hasBarColor && !barOnRight,
   'rs-px-3': !hasBarColor,
 });
