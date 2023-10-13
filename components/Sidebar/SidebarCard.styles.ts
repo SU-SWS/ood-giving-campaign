6317a89ab273d87a45cc689588c708bfc2f64276
Yvonne Tang <ytang2@stanford.edu>
2023-10-13 11:07:33 -0700

    container query for more padding if sidebar is wider

--- components/Sidebar/SidebarCard.styles.ts
+++ components/Sidebar/SidebarCard.styles.ts
@@ -11,11 +11,11 @@ export const content = (
   'rs-py-2': !hasBgColor,
   'pr-0': !hasBgColor && hasBarColor && !barOnRight,
   'pl-0': !hasBgColor && hasBarColor && barOnRight,
-  'rs-pl-2 @2xl:pl-95': hasBgColor && hasBarColor && barOnRight,
-  'rs-pr-2 @2xl:pr-95': hasBgColor && hasBarColor && !barOnRight,
+  'rs-pl-2 @sm:pl-48 @2xl:pl-95': hasBgColor && hasBarColor && barOnRight,
+  'rs-pr-2 @sm:pr-48 @2xl:pr-95': hasBgColor && hasBarColor && !barOnRight,
   'border-l-[1.4rem] 2xl:border-l-[2rem] rs-pl-1 @2xl:pl-75': hasBarColor && !barOnRight,
   'border-r-[1.4rem] 2xl:border-r-[2rem] rs-pr-1 @2xl:pr-75': hasBarColor && barOnRight,
-  'rs-px-2': !hasBarColor && hasBgColor,
+  'rs-px-2 @sm:px-48': !hasBarColor && hasBgColor,
   'px-0': !hasBarColor && !hasBgColor,
 });
 
