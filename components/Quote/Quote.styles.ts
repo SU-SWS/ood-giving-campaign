51f2df486de4d3b5bb4b8c7c8a90eb1c31f80a3d
Yvonne Tang <ytang2@stanford.edu>
2023-10-14 01:07:58 -0700

    Add optional border options

--- components/Quote/Quote.styles.ts
+++ components/Quote/Quote.styles.ts
@@ -1,1 +1,8 @@
+import { cnb } from 'cnbuilder';
+
 export const root = 'relative max-w-[60ch]';
+export const content = (hasBarColor?: boolean, quoteOnRight?: boolean) => cnb('', {
+  '' : !hasBarColor,
+  'border-r-[1.4rem] 2xl:border-r-[2rem] rs-pr-2' : hasBarColor && !quoteOnRight,
+  'border-l-[1.4rem] 2xl:border-l-[2rem] rs-pl-2' : hasBarColor && quoteOnRight,
+});
--- components/Quote/Quote.tsx
+++ components/Quote/Quote.tsx
@@ -48,35 +48,37 @@ export const Quote = ({
       className={cnb(styles.root, className)}
       {...props}
     >
-      {teaser && (
-        <FlexBox className="w-fit gap-9" direction={quoteOnRight ? 'row-reverse' : 'row'}>
+      <div className={cnb(styles.content(!!barColor, quoteOnRight), accentBorderColors[barColor])}>
+        {teaser && (
+          <FlexBox className="w-fit gap-9" direction={quoteOnRight ? 'row-reverse' : 'row'}>
+            <Text
+              font="druk"
+              className={cnb(
+                'shrink-0 leading-[0]', isLargeTeaser ? 'text-[32rem] mt-[11.4rem]' : 'text-[26rem] mt-[9.2rem]',
+                accentTextColors[quoteColor])}
+            >
+              {quoteOnRight ? '”' : '“'}
+            </Text>
+            <Text size={isLargeTeaser ? 'f5' : 'f4'} leading="none" font="druk" className="rs-mb-0 grow-0 w-fit">{teaser}</Text>
+          </FlexBox>
+        )}
+        {body && (
           <Text
-            font="druk"
-            className={cnb(
-              'shrink-0 leading-[0]', isLargeTeaser ? 'text-[32rem] mt-[11.4rem]' : 'text-[26rem] mt-[9.2rem]',
-              accentTextColors[quoteColor])}
+            as="blockquote"
+            weight={isLargeBody ? 'semibold' : 'normal'}
+            variant={isLargeBody ? undefined : 'big'}
+            leading="display"
+            size={isLargeBody ? 2 : undefined}
+            font="serif"
+            className="mt-02em first:mt-0"
           >
-            {quoteOnRight ? '”' : '“'}
+            {body}
           </Text>
-          <Text size={isLargeTeaser ? 'f5' : 'f4'} leading="none" font="druk" className="rs-mb-0 grow-0 w-fit">{teaser}</Text>
-        </FlexBox>
-      )}
-      {body && (
-        <Text
-          as="blockquote"
-          weight={isLargeBody ? 'semibold' : 'normal'}
-          variant={isLargeBody ? undefined : 'big'}
-          leading="display"
-          size={isLargeBody ? 2 : undefined}
-          font="serif"
-          className="mt-02em first:mt-0"
-        >
-          {body}
-        </Text>
-      )}
-      {source && (
-        <Text variant="card" font="serif" className="rs-mt-1 whitespace-pre-line">{source}</Text>
-      )}
+        )}
+        {source && (
+          <Text variant="card" font="serif" className="rs-mt-1 whitespace-pre-line">{source}</Text>
+        )}
+      </div>
     </Container>
   </AnimateInView>
 );
