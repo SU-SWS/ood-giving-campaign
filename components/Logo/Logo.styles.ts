3ef34c6055d2619e1ffbed51d6e0480b3a5f3af7
Yvonne Tang <ytang2@stanford.edu>
2023-10-16 17:52:22 -0700

    more finetune

--- components/Logo/Logo.styles.ts
+++ components/Logo/Logo.styles.ts
@@ -19,7 +19,7 @@ export const root = 'no-underline inline-block font-normal';
 export const contentWrapper = 'flex-col sm:flex-row items-start sm:items-center';
 export const logo = 'text-19 sm:text-[1.43em] leading-half mt-[0.27em]';
 export const bar = 'hidden sm:block w-1 h-1em mx-03em';
-export const text = 'text-15 md:text-[0.95em] sm:text-[1.05em] mt-[0.25em]';
+export const text = 'text-15 sm:text-[1.05em] mt-[0.25em]';
 export const textColor = {
   default: 'text-gc-black',
   white: 'text-white',
--- components/Masthead/Masthead.styles.ts
+++ components/Masthead/Masthead.styles.ts
@@ -1,3 +1,3 @@
 // Pass in a root style for the lock up to scale everything within proportionally
-export const lockup = 'sm:-mt-02em shrink-0 sm:text-[0.95em] md:text-[1.14em]';
+export const lockup = 'sm:-mt-02em shrink-0 sm:text-17 md:text-22 lg:text-[2.8rem]';
 export const ctaWrapper = 'gap-12 xl:gap-20';
