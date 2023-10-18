49be04a98981c52bc1955db2e566a20d3b2e191e
Yvonne Tang <ytang2@stanford.edu>
2023-10-18 14:09:16 -0700

    add className support for ImageOverlay

--- components/ImageOverlay.tsx
+++ components/ImageOverlay.tsx
@@ -8,11 +8,10 @@ export const overlays = {
   'black-70': 'bg-black-true/70',
   'black-80': 'bg-black-true/80',
   'black-gradient': 'bg-gradient-to-b from-transparent via-black-true/20 to-black-true/50',
-  'black-gradient-dark': 'bg-gradient-to-b from-transparent via-black-true/30 to-black-true/80',
+  'black-gradient-dark': 'bg-gradient-to-b from-transparent via-black-true/30 to-black-true/80', // Changemaker cards
   'black-top-bottom': 'bg-gradient-to-b from-gc-black via-transparent via-40% to-gc-black',
-  'black-top-sm': 'bg-gradient-to-b from-gc-black via-periwinkle via-20% to-transparent',
   'white-90': 'bg-white/90',
-  'homepage-hero': 'bg-gradient-to-b from-[#263588] to-transparent',
+  'homepage-hero': 'bg-gradient-to-b from-[#182671]',
 };
 export type OverlayType = keyof typeof overlays | '';
 
@@ -36,7 +35,7 @@ export const ImageOverlay = ({
       src={imageSrc}
       alt=""
       loading={loading}
-      className="absolute w-full h-full object-cover top-0 left-0"
+      className={cnb('absolute w-full h-full object-cover top-0 left-0', className)}
       {...props}
     />
     {overlay && (
