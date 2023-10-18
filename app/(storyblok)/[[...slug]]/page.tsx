742e35f77de0c6dfdd67b50a6036607e0ab936e0
Yvonne Tang <ytang2@stanford.edu>
2023-10-18 16:45:16 -0700

    Let's try this for cache

--- app/(storyblok)/[[...slug]]/page.tsx
+++ app/(storyblok)/[[...slug]]/page.tsx
@@ -58,7 +58,7 @@ async function getStoryData(params: { slug: string[] }) {
   let slug = params.slug ? params.slug.join('/') : 'home';
   let sbParams: ISbStoriesParams = {
     version: activeEnv === 'development' ? 'draft' : 'published',
-    cv: activeEnv === 'development' ? Date.now() : undefined,
+    cv: Date.now(),
     resolve_relations: resolveRelations,
   };
   const storyblokApi: StoryblokClient = getStoryblokApi();
