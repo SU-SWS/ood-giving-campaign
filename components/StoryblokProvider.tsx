935e4b62a4663bb9520c4f06c6fcd9b2de66a25b
Shea McKinney <sheamck@stanford.edu>
2023-10-24 14:33:31 -0700

    GIVCAMP-242: Change to public token for client components.

--- components/StoryblokProvider.tsx
+++ components/StoryblokProvider.tsx
@@ -56,7 +56,7 @@ const components = {
 };
 
 storyblokInit({
-  accessToken: process.env.STORYBLOK_ACCESS_TOKEN,
+  accessToken: process.env.NEXT_PUBLIC_STORYBLOK_ACCESS_TOKEN,
   use: [apiPlugin],
   apiOptions: {
     region: 'us',
