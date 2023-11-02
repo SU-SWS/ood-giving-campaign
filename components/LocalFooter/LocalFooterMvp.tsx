d37d92b493a17b37a1decff41dae892682f564ef
Yvonne Tang <ytang2@stanford.edu>
2023-11-02 16:13:09 -0700

    Local footer and Together section changes

--- components/LocalFooter/LocalFooterMvp.tsx
+++ components/LocalFooter/LocalFooterMvp.tsx
@@ -8,21 +8,29 @@ import { FlexBox } from '../FlexBox';
 import * as links from '@/utilities/externalLinks';
 
 export const LocalFooterMvp = () => (
-  <Container as="nav" bgColor="black" py={10} aria-label="Local footer menu">
-    <div className="w-full">
+  <Container
+    as="nav"
+    bgColor="black"
+    py={10}
+    aria-label="Local footer menu"
+    className="relative overflow-hidden bg-no-repeat bg-bottom bg-contain"
+    style={{ backgroundImage: `url('${getProcessedImage('https://a-us.storyblok.com/f/1005200/3200x2134/f8433eac94/fdf67c168f2e36a6bc30c085eb392c13.jpg', '2000x0')}')` }}
+  >
+    <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-black-true/60" />
+    <div className="relative z-10 w-full">
       <LogoLockup text="Impact Stories" color="white" isLink className="text-[1.6em]" />
-      <FlexBox wrap="wrap" className="list-unstyled divide-x divide-white children:inline-block children:mb-0 children:px-16 md:children:px-48 children:leading-display first:children:pl-0 last:children:pr-0 mx-auto w-fit rs-mt-2 gap-y-10">
-        <li><CtaLink color="white" href="faq" className="inline-block" size="large">Giving to Stanford</CtaLink></li>
-        <li><CtaLink color="white" href="contact-us" className="inline-block" size="large">Contact us</CtaLink></li>
-        <li><CtaLink color="white" href="contact-us" className="inline-block" size="large">Leadership</CtaLink></li>
-        <li><CtaLink color="white" href="contact-us" className="inline-block" size="large">How to make a gift</CtaLink></li>
+      <FlexBox wrap="wrap" className="list-unstyled divide-x divide-white children:inline-block children:mb-0 children:px-16 md:children:px-30 xl:children:px-48 children:leading-display first:children:pl-0 last:children:pr-0 lg:mx-auto w-fit rs-mt-2 gap-y-10">
+        <li><CtaLink color="white" href={links.ood.giving} size="large">Giving to Stanford</CtaLink></li>
+        <li><CtaLink color="white" href={links.ood.contact} size="large">Contact us</CtaLink></li>
+        <li><CtaLink color="white" href={links.ood.leadership} size="large">Leadership</CtaLink></li>
+        <li><CtaLink color="white" href={links.ood.waysToGive} size="large">How to make a gift</CtaLink></li>
       </FlexBox>
       <Grid lg={2} gap="default" pt={8}>
         <div>
           <Heading as="h3" size={3}>
             Get the latest in your inbox
           </Heading>
-          <Paragraph variant="big">
+          <Paragraph variant="big" leading="display">
             Sign up to receive Stanford’s storytelling newsletter.
           </Paragraph>
           <CtaLink
@@ -34,11 +42,11 @@ export const LocalFooterMvp = () => (
             Sign up
           </CtaLink>
         </div>
-        <div>
+        <div className="rs-mt-3 lg:mt-0">
           <Heading as="h3" size={3}>
             We can do this, together
           </Heading>
-          <Paragraph variant="big">
+          <Paragraph variant="big" leading="display">
             With your support, this vision will become reality.
           </Paragraph>
           <CtaLink
--- utilities/externalLinks.ts
+++ utilities/externalLinks.ts
@@ -6,6 +6,8 @@
 export const ood = {
   give: 'https://give.stanford.edu/',
   giving: 'https://giving.stanford.edu/',
+  contact: 'https://giving.stanford.edu/contact/',
+  leadership: 'https://TODO', // Alexis will make a new page on Giving
   newsletterSignUp: 'https://TODO',
   waysToGive: 'https://giving.stanford.edu/how-to-make-a-gift/',
 };
