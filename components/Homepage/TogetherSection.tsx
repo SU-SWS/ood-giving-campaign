8ed95ff957b57aef984f36cc40016f960912c7f4
Yvonne Tang <ytang2@stanford.edu>
2023-11-02 09:55:47 -0700

    Adjustment to brand video section

--- components/Homepage/TogetherSection.tsx
+++ components/Homepage/TogetherSection.tsx
@@ -14,13 +14,14 @@ export const TogetherSection = () => {
     <Container pb={7} width="full" bgColor="black" className="relative overflow-hidden">
       <Container>
         <AnimateInView animation="slideUp">
-          <Heading size="f6" align="center" leading="tight" className="max-w-1200 mx-auto">
-            True change happens when we come together.
+          <Heading size="f6" align="center" leading="tight" className="mx-auto max-w-[105rem]">
+            True change will require all of us.
           </Heading>
         </AnimateInView>
         <AnimateInView animation="slideUp" delay={0.2}>
           <Paragraph leading="display" align="center" size={2} className="max-w-900 mx-auto">
             One changemaker is admirable. A community of changemakers is unstoppable.
+            There is nothing we can do that we cannot do better...
           </Paragraph>
         </AnimateInView>
       </Container>
@@ -40,16 +41,6 @@ export const TogetherSection = () => {
           alt="Animated view of the Stanford Dish"
           className="w-[86vw] sm:w-[60vw] xl:w-[42vw] mx-auto -mt-[13vw]"
         />
-        <Paragraph
-          font="serif"
-          italic
-          leading="display"
-          align="center"
-          size={2}
-          className="max-w-1000 mx-auto rs-mt-5"
-        >
-          There is nothing we can do that we cannot do better, together.
-        </Paragraph>
       </Container>
     </Container>
   );
