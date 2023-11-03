34ab33b4db1dee72e279bae2799c761e0fe63d90
Yvonne Tang <42749717+yvonnetangsu@users.noreply.github.com>
2023-11-03 01:38:56 -0700

    GIVCAMP-230 | Together section animation and other misc updates (#157)
    * Ali wants logo lines to line up with footer CTA dividers
    
    * Animation Together
    
    * linter
    
    * Animation continued
    
    * Changemaker cards update

--- components/LocalFooter/LocalFooterMvp.styles.tsx
+++ components/LocalFooter/LocalFooterMvp.styles.tsx
@@ -1,7 +1,7 @@
 export const root = 'relative overflow-hidden bg-no-repeat bg-bottom bg-cover md:bg-contain';
-export const overlay = 'absolute top-0 left-0 w-full h-full bg-gradient-to-b from-black-true via-black-true/80 to-black-true/40';
+export const overlay = 'absolute top-0 left-0 w-full h-full bg-gradient-to-b from-gc-black via-gc-black/80 to-gc-black/40';
 export const flexWrapper = 'items-start lg:items-center relative z-10 w-full';
-export const logo = 'scale-125 origin-left sm:scale-100 text-[1.6em]';
+export const logo = 'scale-125 origin-left sm:scale-100 text-[1.67em] md:text-[1.72em] lg:text-[1.9em] lg:ml-[5.4rem] 2xl:ml-60';
 export const ul = 'list-unstyled divide-x divide-white children:inline-block children:mb-0 children:px-16 md:children:px-30 xl:children:px-48 children:leading-display first:children:pl-0 last:children:pr-0 lg:mx-auto w-fit rs-mt-2 gap-y-10';
 export const grid = 'w-full lg:gap-x-[7vw] 3xl:gap-x-300';
 export const column2 = 'rs-mt-3 lg:mt-0';
