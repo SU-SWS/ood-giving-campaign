3aae09d410720cbd18f3917eaba4bc60f3fa3df4
Yvonne Tang <ytang2@stanford.edu>
2023-11-02 17:01:24 -0700

    move styles to styles file

--- components/LocalFooter/LocalFooterMvp.tsx
+++ components/LocalFooter/LocalFooterMvp.tsx
@@ -6,6 +6,7 @@ import { CtaLink } from '../Cta';
 import { getProcessedImage } from '@/utilities/getProcessedImage';
 import { FlexBox } from '../FlexBox';
 import * as links from '@/utilities/externalLinks';
+import * as styles from './LocalFooterMvp.styles';
 
 export const LocalFooterMvp = () => (
   <Container
@@ -13,20 +14,19 @@ export const LocalFooterMvp = () => (
     bgColor="black"
     py={10}
     aria-label="Local footer menu"
-    className="relative overflow-hidden bg-no-repeat bg-bottom bg-cover md:bg-contain"
+    className={styles.root}
     style={{ backgroundImage: `url('${getProcessedImage('https://a-us.storyblok.com/f/1005200/3200x2134/f8433eac94/fdf67c168f2e36a6bc30c085eb392c13.jpg', '2000x0')}')` }}
   >
-    {/* <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-black-true/60" /> */}
-    <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-black-true via-black-true/80 to-black-true/40" />
-    <FlexBox direction="col" className="items-start lg:items-center relative z-10 w-full">
-      <LogoLockup text="Impact Stories" color="white" isLink className="scale-125 origin-left sm:scale-100 text-[1.6em]" />
-      <FlexBox wrap="wrap" className="list-unstyled divide-x divide-white children:inline-block children:mb-0 children:px-16 md:children:px-30 xl:children:px-48 children:leading-display first:children:pl-0 last:children:pr-0 lg:mx-auto w-fit rs-mt-2 gap-y-10">
+    <div className={styles.overlay} />
+    <FlexBox direction="col" className={styles.flexWrapper}>
+      <LogoLockup text="Impact Stories" color="white" isLink className={styles.logo} />
+      <FlexBox as="ul" wrap="wrap" className={styles.ul}>
         <li><CtaLink color="white" href={links.ood.giving} size="large">Giving to Stanford</CtaLink></li>
         <li><CtaLink color="white" href={links.ood.contact} size="large">Contact us</CtaLink></li>
         <li><CtaLink color="white" href={links.ood.leadership} size="large">Leadership</CtaLink></li>
         <li><CtaLink color="white" href={links.ood.waysToGive} size="large">How to make a gift</CtaLink></li>
       </FlexBox>
-      <Grid lg={2} pt={8} gap="default" className="w-full lg:gap-x-[7vw] 3xl:gap-x-300">
+      <Grid lg={2} pt={8} gap="default" className={styles.grid}>
         <div>
           <Heading as="h3" size={3}>
             Get the latest in your inbox
@@ -43,7 +43,7 @@ export const LocalFooterMvp = () => (
             Sign up
           </CtaLink>
         </div>
-        <div className="rs-mt-3 lg:mt-0">
+        <div className={styles.column2}>
           <Heading as="h3" size={3}>
             We can do this, together
           </Heading>
