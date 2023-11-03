15a5a2673bf3dbfe1d5074371824d68369a92028
Yvonne Tang <ytang2@stanford.edu>
2023-11-02 18:56:01 -0700

    Animation Together

--- components/Homepage/TogetherSection.tsx
+++ components/Homepage/TogetherSection.tsx
@@ -1,17 +1,27 @@
 'use client';
 
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import {
   m, useScroll, useSpring, useTransform, SpringOptions, useWillChange,
 } from 'framer-motion';
 import { AnimateInView } from '../Animate';
 import { Container } from '../Container';
-import { Heading, Text, Paragraph } from '../Typography';
+import { Heading, Paragraph } from '../Typography';
 import { getProcessedImage } from '@/utilities/getProcessedImage';
 
 export const TogetherSection = () => {
+  const sectionRef = useRef<HTMLDivElement>(null);
+  const willChange = useWillChange();
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ['-80vh', '0'],
+  });
+  const trackingAnimation = useTransform(scrollYProgress, [0, 1], ['0.3em', '0.025em']);
+  const scaleAnimation = useTransform(scrollYProgress, [0, 1], [3, 1]);
+  const opacityAnimation = useTransform(scrollYProgress, [0, 1], [0, 1]);
+
   return (
-    <Container pb={7} width="full" bgColor="black" className="relative overflow-hidden">
+    <div ref={sectionRef} className="relative overflow-hidden rs-pb-8 w-full bg-gc-black text-white">
       <Container>
         <AnimateInView animation="slideUp">
           <Heading size="f6" align="center" leading="tight" className="mx-auto max-w-[105rem]">
@@ -25,23 +35,20 @@ export const TogetherSection = () => {
           </Paragraph>
         </AnimateInView>
       </Container>
-      <Text
-        font="druk"
-        color="white"
-        align="center"
-        leading="none"
+      <m.div
         aria-hidden
-        className="text-[28vw] -ml-[0.04em] tracking-wide"
+        className="font-druk uppercase text-center leading-none text-[28vw] -ml-[0.04em] tracking-wide z-0"
+        style={{ scale: scaleAnimation, opacity: opacityAnimation, letterSpacing: trackingAnimation, willChange }}
       >
         Together
-      </Text>
+      </m.div>
       <Container>
         <img
-          src={getProcessedImage('https://a-us.storyblok.com/f/1005200/3014x1979/5d314e43ba/together_h412-18.jpg', '1000x0')}
+          src={getProcessedImage('https://a-us.storyblok.com/f/1005200/3014x1979/5d314e43ba/together_h412-18.jpg', '1200x0')}
           alt=""
-          className="w-[86vw] sm:w-[60vw] xl:w-[42vw] mx-auto -mt-[13vw]"
+          className="relative w-[86vw] sm:w-[60vw] xl:w-[42vw] mx-auto -mt-[13vw] z-10"
         />
       </Container>
-    </Container>
+    </div>
   );
 };
