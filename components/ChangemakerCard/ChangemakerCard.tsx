e856baade80d75e474f1a566e4f6661bc7e70c4d
Yvonne Tang <ytang2@stanford.edu>
2023-11-08 11:21:00 -0800

    Start changing flipping to onClick and a11y

--- components/ChangemakerCard/ChangemakerCard.tsx
+++ components/ChangemakerCard/ChangemakerCard.tsx
@@ -1,8 +1,8 @@
 import { cnb } from 'cnbuilder';
-import {AnimateInView, type AnimationType } from '../Animate';
-import { CtaButton } from '../Cta/CtaButton';
+import { useRef, useState } from 'react';
+import { AnimateInView, type AnimationType } from '../Animate';
 import {
-  Heading, type HeadingType, Paragraph, Text,
+  Heading, type HeadingType, Text,
 } from '../Typography';
 import { FlexBox } from '../FlexBox';
 import { HeroIcon } from '../HeroIcon';
@@ -38,56 +38,95 @@ export const ChangemakerCard = ({
   children,
   className,
   ...props
-}: ChangemakerCardProps) => (
-  <AnimateInView animation={animation} delay={delay}>
-    <article
-      className={cnb(styles.root, className)}
-      tabIndex={0}
-      {...props}
-    >
-      <div className={styles.cardInner}>
-        {/* Front of the card */}
-        <div className={styles.cardFront}>
-          {imageSrc && (
-            <div className={cnb(styles.imageWrapper)}>
-              <ImageOverlay
-                imageSrc={getProcessedImage(imageSrc, '500x1000', imageFocus)}
-                overlay="black-gradient-dark"
-              />
+}: ChangemakerCardProps) => {
+  const cardInnerRef = useRef<HTMLDivElement>(null);
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  // Toggle the card's flipped state and update isFlipped state
+  const toggleCard = () => {
+    setIsFlipped(!isFlipped);
+  };
+
+  // Onclick function to rotate cardInnerRef by 180 degrees in the Y axis
+  const flipCard = (degrees: number) => {
+    if (cardInnerRef.current) {
+      cardInnerRef.current.style.transform = `rotateY(${degrees}deg)`;
+      toggleCard();
+    }
+  };
+
+  return (
+    <AnimateInView animation={animation} delay={delay}>
+      <article
+        className={cnb(styles.root, className)}
+        {...props}
+      >
+        <div ref={cardInnerRef} className={styles.cardInner}>
+          {/* Front of the card */}
+          <div className={styles.cardFront}>
+            {imageSrc && (
+              <div className={cnb(styles.imageWrapper)}>
+                <ImageOverlay
+                  imageSrc={getProcessedImage(imageSrc, '500x1000', imageFocus)}
+                  overlay="black-gradient-dark"
+                />
+              </div>
+            )}
+            <div className={styles.content}>
+              {heading && (
+                <Heading
+                  as={headingLevel}
+                  size={3}
+                  leading="tight"
+                  align="center"
+                  className={styles.heading}
+                >
+                  {heading}
+                </Heading>
+              )}
+              {body && (
+                <Text variant="card" align="center">{body}</Text>
+              )}
+              <FlexBox direction="col" alignItems="center" className="absolute bottom-50 right-36 text-white">
+                <button
+                  type="button"
+                  onClick={() => flipCard(180)}
+                  aria-label={`Flip card to read more about ${heading}`}
+                >
+                  <HeroIcon
+                    noBaseStyle
+                    icon='plus'
+                    className="w-65 h-65 border-2 border-white rounded-full p-16"
+                  />
+                </button>
+                <Text as="span" font="serif" color="white" variant="caption">
+                  {ctaLabel}
+                </Text>
+              </FlexBox>
             </div>
-          )}
-          <div className={styles.content}>
-            {heading && (
-              <Heading
-                as={headingLevel}
-                size={3}
-                leading="tight"
-                align="center"
-                className={styles.heading}
-              >
-                {heading}
-              </Heading>
-            )}
-            {body && (
-              <Paragraph variant="card" align="center" noMargin>{body}</Paragraph>
-            )}
+          </div>
+          {/* Back of the card */}
+          <FlexBox
+            direction="col"
+            className={styles.cardBack}
+            aria-hidden={!isFlipped}
+          >
+            {children}
             <FlexBox direction="col" alignItems="center" className="absolute bottom-50 right-36 text-white">
-              <HeroIcon
-                noBaseStyle
-                icon='plus'
-                className="w-65 h-65 border-2 border-white rounded-full p-16"
-              />
+              <button type="button" onClick={() => flipCard(0)} aria-label="Dismiss">
+                <HeroIcon
+                  noBaseStyle
+                  icon='plus'
+                  className="w-65 h-65 border-2 border-white rounded-full p-16 rotate-45"
+                />
+              </button>
               <Text as="span" font="serif" color="white" variant="caption">
                 {ctaLabel}
               </Text>
             </FlexBox>
-          </div>
+          </FlexBox>
         </div>
-        {/* Back of the card */}
-        <FlexBox direction="col" justifyContent="center" className={styles.cardBack}>
-          {children}
-        </FlexBox>
-      </div>
-    </article>
-  </AnimateInView>
-);
+      </article>
+    </AnimateInView>
+  );
+};
