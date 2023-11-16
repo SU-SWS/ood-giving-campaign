9f946c23ca6e98c7bdef11492c1f2d054b1bc1c3
Shea McKinney <sheamck@stanford.edu>
2023-11-15 16:05:06 -0800

    GIVCAMP-164: Move external link click to CTA component.

--- components/GAProvider.tsx
+++ components/GAProvider.tsx
@@ -1,105 +1,19 @@
 'use client';
 import { useEffect } from 'react';
-import { setCookie, getCookie } from 'cookies-next';
-import { get } from 'http';
+import useUTMs from '@/hooks/useUTMs';
 
 export default function GAProvider({ children }: { children: React.ReactNode }) {
-  const cookieName = 'SU-GC-UTMs';
+  const { setUTMCookie, deleteUTMCookie } = useUTMs();
 
   useEffect(() => {
     if (typeof window === 'undefined') {
       return;
     }
-
-    const urlParams = new URLSearchParams(window.location.search);
-    const utm_source = urlParams.get('utm_source') || undefined;
-    const utm_medium = urlParams.get('utm_medium') || undefined;
-    const utm_campaign = urlParams.get('utm_campaign') || undefined;
-    const utm_content = urlParams.get('utm_content') || undefined;
-    const utm_term = urlParams.get('utm_term') || undefined;
-    const UTMs = {
-      utm_source,
-      utm_medium,
-      utm_campaign,
-      utm_content,
-      utm_term,
+    setUTMCookie();
+    return () => {
+      deleteUTMCookie();
     };
-
-    // UTM source is a required param for GA to work.
-    if (utm_source) {
-      setCookie(cookieName, UTMs, {
-        path: '/',
-        domain: window.location.hostname,
-        secure: window.location.protocol === 'https' ? true : false,
-        httpOnly: false,
-        sameSite: 'strict',
-      });
-    }
-
-    // Function to get the active anchor element that was clicked on if there is one.
-    const getActiveElement = (event: MouseEvent):Element | null => {
-
-      const target = event.target;
-      if (target instanceof Element === false) { return null; }
-
-      // If the target is a link.
-      if ((target as Element).tagName === 'A') { return target as Element; }
-
-      // Start with the parent element of the target
-      let parentElement = (target as Element).parentElement;
-
-      // Loop through the parent elements until an anchor element is found or parent becomes null
-      while (parentElement !== null) {
-        // Check if the current parent element is an anchor element
-        if (parentElement.tagName === 'A') {
-          return parentElement; // Found an anchor element, so return it
-        }
-
-        parentElement = parentElement.parentElement; // Move up to the next parent element
-      }
-
-      return null; // No anchor element found in the parent elements
-    };
-
-    // Listen to all of the click events and add the UTM params to the outgoing location.
-    document.addEventListener('click', (e) => {
-      const cookie = getCookie(cookieName);
-      if (!cookie) { return;}
-
-      const utms = JSON.parse(cookie);
-      if (!utms.utm_source) { return;}
-
-      // Get the element that was clicked on and make sure it is a link.
-      let activeElement:Element;
-      if (document.activeElement === document.body) { activeElement = getActiveElement(e); }
-      if (!activeElement && document.activeElement instanceof Element && 'tagName' in document.activeElement) { activeElement = document.activeElement; }
-
-      const isLink = activeElement && activeElement.tagName === 'A';
-      if (!isLink) { return;}
-
-      // Make sure it has an href.
-      const href = activeElement.getAttribute('href');
-      if (!href) { return;}
-
-      // check if the href is an external link.
-      if (!href.startsWith('http')) { return; }
-      if (href.startsWith(window.location.origin)) { return; }
-
-      // Add the UTM params to the href before heading out.
-      const url = new URL(href);
-      !!utms.utm_source && !url.searchParams.get('utm_source') && url.searchParams.set('utm_source', utms.utm_source);
-      !!utms.utm_medium && !url.searchParams.get('utm_medium') && url.searchParams.set('utm_medium', utms.utm_medium);
-      !!utms.utm_campaign && !url.searchParams.get('utm_campaign') && url.searchParams.set('utm_campaign', utms.utm_campaign);
-      !!utms.utm_content && !url.searchParams.get('utm_content') && url.searchParams.set('utm_content', utms.utm_content);
-      !!utms.utm_term && !url.searchParams.get('utm_term') && url.searchParams.set('utm_term', utms.utm_term);
-
-      // Take over the click event and send the user to the new location.
-      e.preventDefault();
-      window.location.replace(url);
-    });
-
-
-  }, []);
+  }, [setUTMCookie, deleteUTMCookie]);
 
   // Pass through children.
   return children;
