2a7eeca095ab29dd3017fc52781c1328319a605c
Yvonne Tang <ytang2@stanford.edu>
2023-11-29 11:21:09 -0800

    Fixup SbThemeCard

--- components/Storyblok/SbThemeCard.tsx
+++ components/Storyblok/SbThemeCard.tsx
@@ -1,13 +1,21 @@
 import { storyblokEditable } from '@storyblok/react/rsc';
 import { ThemeCard } from '../VerticalCard';
-import { type SbStoryCardProps } from './SbStoryCard';
-import { paletteAccentColors } from '@/utilities/colorPalettePlugin';
+import { type AnimationType } from '../Animate';
+import { type HeadingType } from '../Typography';
+import { type SbImageType, type SbLinkType } from './Storyblok.types';
 
-export type SbThemeCardProps = Omit<SbStoryCardProps, 'isSmallHeading'> & {
+export type SbThemeCardProps = {
   blok: {
+    _uid: string;
+    heading?: string;
+    headingLevel?: HeadingType;
     body?: string;
+    image?: SbImageType;
+    link?: SbLinkType;
     ctaLabel?: string;
     ctaSrText?: string;
+    animation?: AnimationType;
+    delay?: number;
   };
 };
 
@@ -17,7 +25,6 @@ export const SbThemeCard = ({
     headingLevel,
     body,
     image: { filename, focus } = {},
-    tabColor: { value } = {},
     ctaLabel,
     ctaSrText,
     link,
@@ -33,7 +40,6 @@ export const SbThemeCard = ({
     body={body}
     imageSrc={filename}
     imageFocus={focus}
-    tabColor={paletteAccentColors[value]}
     ctaLabel={ctaLabel}
     ctaSrText={ctaSrText}
     link={link}
