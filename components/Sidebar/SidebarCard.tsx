23bd09ea68b3f63fc7d93365b311c159707d89f9
Yvonne Tang <ytang2@stanford.edu>
2023-12-06 10:58:26 -0800

    Add backdrop blur to sidebar card

--- components/Sidebar/SidebarCard.tsx
+++ components/Sidebar/SidebarCard.tsx
@@ -1,7 +1,9 @@
 import { cnb } from 'cnbuilder';
 import { AnimateInView, type AnimationType } from '../Animate';
 import { Container } from '../Container';
-import { Heading, type HeadingType, Text } from '../Typography';
+import {
+  Heading, type HeadingType, SrOnlyText, Text,
+} from '../Typography';
 import {
   accentBorderColors,
   accentBgColors,
@@ -54,7 +56,7 @@ export const SidebarCard = ({
         )}
         {heading && (
           <Heading as={headingLevel} size={isSmallHeading ? 2 : 3} className={styles.heading}>
-            {superhead && <Text srOnly>{superhead}: </Text>}{heading}
+            {superhead && <SrOnlyText>{`${superhead}: `}</SrOnlyText>}{heading}
           </Heading>
         )}
         {children}
