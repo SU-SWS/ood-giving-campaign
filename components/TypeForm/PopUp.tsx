f41167c777086be4a6bf24ae81be836b113c6ff6
Yvonne Tang <ytang2@stanford.edu>
2023-12-12 14:18:23 -0800

    Add button type to Typeform popup button

--- components/TypeForm/PopUp.tsx
+++ components/TypeForm/PopUp.tsx
@@ -1,19 +1,18 @@
 'use client';
 import { PopupButton } from '@typeform/embed-react';
 import { type PopupOptions } from '@typeform/embed/types';
-import { cnb, type ClassValue } from 'cnbuilder';
 
 export type PopUpProps = PopupOptions & {
   id: string;
   children: React.ReactNode;
-  className?: ClassValue;
+  className?: string;
 }
 
 const PopUp = ({
   id, children, className, ...rest
 }:PopUpProps) => {
   return (
-    <PopupButton noHeading keepSession id={id} className={cnb(className)} {...rest}>
+    <PopupButton buttonProps={{ type: 'button' }} noHeading keepSession id={id} className={className} {...rest}>
       {children}
     </PopupButton>
   );
