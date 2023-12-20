89d65f871e3e12e2347dcb9ed87b531cf7a11628
Shea McKinney <sheamck@stanford.edu>
2023-12-20 12:58:32 -0800

    fixup!

--- app/(storyblok)/editor/page.tsx
+++ app/(storyblok)/editor/page.tsx
@@ -28,7 +28,7 @@ type PageProps = {
 // Control what happens when a dynamic segment is visited that was not generated with generateStaticParams.
 export const dynamicParams = false;
 export const dynamic = 'force-dynamic';
-export const revalidate = 'force-cache';
+export const revalidate = 0;
 
 // Storyblok bridge options.
 const bridgeOptions = {
