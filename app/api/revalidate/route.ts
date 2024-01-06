ecd84f1fe613f05e17c67f3361856a1d2e110f79
Shea McKinney <sheamck@stanford.edu>
2024-01-05 17:47:36 -0800

    Trying something for Safari.

--- app/api/revalidate/route.ts
+++
@@ -1,9 +0,0 @@
-import { revalidatePath } from 'next/cache';
-
-export const dynamic = 'force-dynamic';
-
-// NextJs app directory revalidate api route
-export async function GET() {
-  const data = revalidatePath('/');
-  return Response.json('ok');
-}
