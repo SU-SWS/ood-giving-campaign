ea8fc7a5d979d3fd2f279fbec9840fc1c1c6cbeb
Shea McKinney <sheamck@stanford.edu>
2024-01-08 12:27:36 -0800

    Netlify clear all caches on deploy.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   experimental: {
     logging: 'verbose',
+    isrMemoryCacheSize: 0, // disable ISR cache in memory and rely on file system cache.
   },
   images: {
     remotePatterns: [
