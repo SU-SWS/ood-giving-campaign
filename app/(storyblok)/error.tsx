193c21b3124251ab4347d2a969ee2441c94e3f54
Shea McKinney <sheamck@stanford.edu>
2024-01-10 16:41:12 -0800

    Remove unused import.

--- app/(storyblok)/error.tsx
+++ app/(storyblok)/error.tsx
@@ -2,10 +2,7 @@
 
 import { useEffect } from 'react';
 import { Masthead } from '@/components/Masthead';
-import { FlexBox } from '@/components/FlexBox';
 import { Container } from '@/components/Container';
-import DynamicLoad from 'next/dynamic';
-const Skeleton = DynamicLoad(() => import('react-loading-skeleton'), { ssr: false });
 
 export default function Error({error, reset}: {
   error: Error & { digest?: string }
