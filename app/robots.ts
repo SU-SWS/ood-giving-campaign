319c511c5c7cc0c6dd6d9da6a964576ceb4deb7b
Sherakama <sheamck@stanford.edu>
2024-03-21 11:30:23 -0700

    Update robots to use different URL for sitemap path (#254)

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-  const CurrentURL = process.env.DEPLOY_PRIME_URL || 'http://localhost:3000';
+  const CurrentURL = process.env.URL || process.env.DEPLOY_PRIME_URL || 'https://momentum.stanford.edu';
   return {
     rules: {
       userAgent: '*',
