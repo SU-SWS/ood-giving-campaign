412a9b5953ecb74f3275dfbb2ca4f788e903aa5f
Yvonne Tang <ytang2@stanford.edu>
2024-04-08 10:38:53 -0700

    caption fixup

--- components/EmbedMedia/EmbedMedia.styles.ts
+++ components/EmbedMedia/EmbedMedia.styles.ts
@@ -1,4 +1,4 @@
 export const mediaWrapper = '*:!w-full *:!h-full';
-export const caption = '*:*:leading-display caption mt-1em max-w-prose-wide';
+export const caption = '*:*:leading-display mt-1em max-w-prose-wide';
 export const iconWrapper = 'size-50 sm:size-70 bg-black-true/70 border-3 rounded-full border-white group-hocus-within:scale-110 transition group-hocus-within:bg-digital-red';
 export const previewIcon = 'text-white size-24 sm:size-30';
--- components/VerticalPoster/VerticalPoster.styles.ts
+++ components/VerticalPoster/VerticalPoster.styles.ts
@@ -52,8 +52,8 @@ export const imageWrapper = (imageOnLeft: boolean) => cnb('w-full bg-gc-black bg
 export const imageInnerWrapper = 'size-full';
 export const image = 'size-full object-cover object-center';
 
-export const caption = (imageOnLeft: boolean) => cnb('relative mt-05em caption *:*:leading-display max-w-prose-wide first:*:*:mt-0 ml-0',
-  imageOnLeft ? '' : 'lg:mr-0 lg:ml-auto lg:*:text-right',
+export const caption = (imageOnLeft: boolean) => cnb('relative mt-05em caption *:*:leading-display *:max-w-prose-wide first:*:*:mt-0 *:ml-0',
+  imageOnLeft ? '' : '*:lg:mr-0 *:lg:ml-auto lg:*:text-right',
 );
 
 // Parallax enabled styles
