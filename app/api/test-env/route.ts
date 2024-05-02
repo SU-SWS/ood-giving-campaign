272a27046036208824d1c8fcb64515a70ebfa1e5
Shea McKinney <sheamck@stanford.edu>
2024-05-02 15:21:57 -0700

    Try try again.

--- app/api/test-env/route.ts
+++ app/api/test-env/route.ts
@@ -4,8 +4,8 @@ import { NextResponse } from 'next/server';
 export const dynamic = 'force-dynamic';
 
 export async function GET(request: Request) {
-  console.log('Next Token:', process.env.NEXT_PUBLIC_STORYBLOK_ACCESS_TOKEN);
+  console.log('Next Token:', process.env.PRODUCTION_STORYBLOK_ACCESS_TOKEN);
   dotenv.config({ path: 'vault.env' });
-  console.log('Next Token AFTER:', process.env.NEXT_PUBLIC_STORYBLOK_ACCESS_TOKEN);
+  console.log('Next Token AFTER:', process.env.PRODUCTION_STORYBLOK_ACCESS_TOKEN);
   return NextResponse.json({ message: 'Hello, World!' });
 }
