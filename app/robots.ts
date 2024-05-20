3492fa23e8931a6e8bc73db2106a9adcf4eb0fff
Yvonne Tang <ytang2@stanford.edu>
2024-05-20 13:46:17 -0700

    Block robots and remove URL from sitemap for /global-components and /test

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
+      disallow: ['/global-components/', '/test/'],
     },
     sitemap: CurrentURL + '/sitemap.xml',
   };
