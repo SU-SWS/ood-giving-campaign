8f7096db2102bfa857fb4719def0fb24522b1bd1
Yvonne Tang <ytang2@stanford.edu>
2024-05-21 15:20:03 -0700

    Light theme page logo switch to white when main nav panel is open using has selector

--- components/Masthead/Masthead.styles.ts
+++ components/Masthead/Masthead.styles.ts
@@ -1,7 +1,7 @@
-export const root = 'w-full absolute top-0 z-50 transition-colors';
+export const root = 'group w-full absolute top-0 z-50 transition-colors';
 
 // Use a wider centered container (1800px wide at 4XL (2000px) breakpoint)
 export const wrapper = 'cc 3xl:px-100 4xl:px-[calc((100%-1800px)/2)] py-12 sm:py-13 lg:py-20 transition';
 
 // Pass in a root style for the lock up to scale everything within proportionally
-export const lockup = 'relative z-[300] sm:-mt-02em shrink-0 sm:text-17 md:text-22 lg:text-28';
+export const lockup = 'relative z-[300] sm:-mt-02em shrink-0 sm:text-17 md:text-22 lg:text-28 group-has-[nav[data-headlessui-state="open"]]:*:*:text-white group-has-[nav[data-headlessui-state="open"]]:even:*:*:bg-white';
