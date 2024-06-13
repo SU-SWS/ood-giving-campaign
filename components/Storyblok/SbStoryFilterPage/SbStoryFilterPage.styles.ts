310194941021778ba27eac5f9eec877a46db60d5
Yvonne Tang <ytang2@stanford.edu>
2024-06-13 13:19:03 -0700

    Add skip link to skip past story filter menu

--- components/Storyblok/SbStoryFilterPage/SbStoryFilterPage.styles.ts
+++ components/Storyblok/SbStoryFilterPage/SbStoryFilterPage.styles.ts
@@ -1,1 +1,3 @@
 export const intro = '*:intro-text *:max-w-prose';
+export const latest = 'relative bg-gc-black';
+export const skiplink = 'focus:block focus:w-fit focus:relative focus:bg-cardinal-red -top-60 focus:top-36 focus:mx-auto';
