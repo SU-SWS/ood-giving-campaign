e5e9972b5cda4943ec4b4b4721d41b4148d3cac5
Yvonne Tang <ytang2@stanford.edu>
2024-07-09 18:02:02 -0700

    Add conditionals

--- components/Homepage/Changemaker.tsx
+++ components/Homepage/Changemaker.tsx
@@ -41,31 +41,41 @@ export const Changemaker = ({
       <div className="absolute top-0 left-0 size-full bg-gradient-to-t from-gc-black to-black/30 lg:to-transparent via-gc-black/40" />
       <Container className="relative z-10">
         <Heading font="serif" leading="tight" align="center" className="fluid-type-6 md:fluid-type-7 max-w-[110rem] mx-auto rs-mb-4">
-          <AnimateInView animation="slideInFromRight">
-            <Text as="span" font="serif" className="block">
-              {changemakerHeading1}
-            </Text>
-          </AnimateInView>
-          <AnimateInView animation="slideInFromLeft" delay={0.1}>
-            <Text as="span" font="serif" className="block">
-              {changemakerHeading2}
-            </Text>
-          </AnimateInView>
-          <AnimateInView animation="slideInFromRight" delay={0.2}>
-            <Text as="span" font="serif" className="block">
-              {changemakerHeading3}
-            </Text>
-          </AnimateInView>
+          {changemakerHeading1 &&
+            <AnimateInView animation="slideInFromRight">
+              <Text as="span" font="serif" className="block">
+                {changemakerHeading1}
+              </Text>
+            </AnimateInView>
+          }
+          {changemakerHeading2 &&
+            <AnimateInView animation="slideInFromLeft" delay={0.1}>
+              <Text as="span" font="serif" className="block">
+                {changemakerHeading2}
+              </Text>
+            </AnimateInView>
+          }
+          {changemakerHeading3 &&
+            <AnimateInView animation="slideInFromRight" delay={0.2}>
+              <Text as="span" font="serif" className="block">
+                {changemakerHeading3}
+              </Text>
+            </AnimateInView>
+          }
         </Heading>
-        <AnimateInView animation="slideUp" delay={0.4}>
-          <Paragraph size={2} weight="semibold" noMargin font="serif" align="center" className="max-w-700 mx-auto rs-my-4 text-balance" leading="display">
-            {changemakerIntro}
-          </Paragraph>
-        </AnimateInView>
+        {changemakerIntro &&
+          <AnimateInView animation="slideUp" delay={0.4}>
+            <Paragraph size={2} weight="semibold" noMargin font="serif" align="center" className="max-w-700 mx-auto rs-my-4 text-balance" leading="display">
+              {changemakerIntro}
+            </Paragraph>
+          </AnimateInView>
+        }
         {changemakerCards}
-        <FlexBox justifyContent="center" className="rs-mt-4">
-          {changemakerCta}
-        </FlexBox>
+        {changemakerCta &&
+          <FlexBox justifyContent="center" className="rs-mt-4">
+            {changemakerCta}
+          </FlexBox>
+        }
       </Container>
     </Container>
   );
