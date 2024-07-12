d61eb174d8697277385aafeb6f9d4ae8087a3069
Yvonne Tang <ytang2@stanford.edu>
2024-07-11 17:21:09 -0700

    Add animation to story filter page h1

--- components/Hero/StoryListHero.styles.ts
+++ components/Hero/StoryListHero.styles.ts
@@ -5,7 +5,7 @@ export const contentWrapper = 'relative z-20 top-0 left-0 size-full *:mx-auto pt
 export const heading = (hasSubhead: boolean) => cnb('mb-0', {
   '3xl:odd:*:text-[20rem] 3xl:even:*:text-[14rem]': !hasSubhead,
 });
-export const subhead = 'text-balance xl:max-w-1100 mx-auto rs-mt-4';
+export const subhead = 'block text-balance xl:max-w-1100 mx-auto rs-mt-4';
 
 export const image = 'absolute top-0 left-0 size-full object-cover';
 export const overlay = 'absolute top-0 left-0 size-full z-10 bg-black-true/60';
