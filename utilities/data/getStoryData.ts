708fa492253f00cd65b5c8699b05a9fd5402d83e
Shea McKinney <sheamck@stanford.edu>
2024-10-10 13:23:10 -0700

    Add cv back to editor.

--- utilities/data/getStoryData.ts
+++ utilities/data/getStoryData.ts
@@ -13,6 +13,7 @@ async function getStoryData({ path, isEditor = false }: getStoryDataProps): Prom
 
   let sbParams: ISbStoriesParams = {
     version: isProd && !isEditor ? 'published' : 'draft',
+    cv: isEditor ? Date.now() : undefined,
     resolve_relations: resolveRelations,
   };
 
