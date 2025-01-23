03a6abb6440d9145dd2b8bfeba094aae5576b99d
Yvonne Tang <ytang2@stanford.edu>
2025-01-22 19:17:54 -0800

    Better doc for getIsSbImagePortrait function

--- utilities/getIsSbImagePortrait.ts
+++ utilities/getIsSbImagePortrait.ts
@@ -1,9 +1,10 @@
-import { getSbImageSize } from './getSbImageSize';
+import { getSbImageSize } from '@/utilities/getSbImageSize';
 
 /**
- * Returns a boolean indicating whether the image is in portrait orientation.
- * @param imageSrc The URL of the Storyblok image.
- * @returns a boolean indicating whether the image is in portrait orientation.
+ * Determines if a Storyblok image is in portrait orientation.
+ *
+ * @param imageSrc - The URL of the Storyblok image.
+ * @returns A boolean indicating whether the image is in portrait orientation.
  */
 export const getIsSbImagePortrait = (imageSrc: string) => {
   const { width, height } = getSbImageSize(imageSrc);
