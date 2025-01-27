31792c862e6ce91900564db675385d476e485dd6
Yvonne Tang <ytang2@stanford.edu>
2025-01-27 10:02:57 -0800

    Dynamically import Sa11y, Lang and SallyLangEn to get rid of the HTMLElement not found error

--- components/Sa11y/Sa11y.tsx
+++ components/Sa11y/Sa11y.tsx
@@ -1,24 +1,27 @@
 'use client';
 
 import { useEffect } from 'react';
-import { Sa11y, Lang } from 'sa11y/dist/js/sa11y.esm.js';
-import Sa11yLangEn from 'sa11y/dist/js/lang/en.js';
 import 'sa11y/dist/css/sa11y.min.css';
 
 export const Sa11yInit: React.FC = () => {
   useEffect(() => {
-    // Ensure Sa11y only runs in the browser
     if (typeof window !== 'undefined') {
-      // Set translations
-      Lang.addI18n(Sa11yLangEn.strings);
+      (async () => {
+        // Dynamically import Sa11y and related dependencies
+        const { Sa11y, Lang } = await import('sa11y/dist/js/sa11y.esm.js');
+        const { default: Sa11yLangEn } = await import('sa11y/dist/js/lang/en.js');
 
-      // Check if the custom element is already defined
-      if (!customElements.get('sa11y-heading-label')) {
-        // Instantiate and run Sa11y
-        const sa11y = new Sa11y.Sa11y({
-          checkRoot: 'body',
-        });
-      }
+        // Set translations
+        Lang.addI18n(Sa11yLangEn.strings);
+
+        // Check if the custom element is already defined
+        if (!customElements.get('sa11y-heading-label')) {
+          // Instantiate and run Sa11y
+          const sa11y = new Sa11y({
+            checkRoot: 'body',
+          });
+        }
+      })();
     }
   }, []);
 
