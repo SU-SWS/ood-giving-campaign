1efaafd3ac7ed7989d56502ee656e077202a8446
Yvonne Tang <42749717+yvonnetangsu@users.noreply.github.com>
2025-01-27 14:22:30 -0800

    DS-1102 | Allow adding custom preview image for Embed Media video light mode (#382)
    * Allow adding custom preview image for light mode for embed videos
    
    * Remove unused previewImageFocus in type

--- components/EmbedMedia/EmbedMedia.tsx
+++ components/EmbedMedia/EmbedMedia.tsx
@@ -1,12 +1,14 @@
-import { useEffect, useState } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
 import { cnb } from 'cnbuilder';
 import ReactPlayer from 'react-player/lazy';
-import { Container } from '../Container';
-import { FlexBox } from '../FlexBox';
-import { WidthBox, type WidthType } from '../WidthBox';
-import { HeroIcon } from '../HeroIcon';
+import { Container } from '@/components/Container';
+import { FlexBox } from '@/components/FlexBox';
+import { WidthBox, type WidthType } from '@/components/WidthBox';
+import { HeroIcon } from '@/components/HeroIcon';
 import { type PaddingType } from '@/utilities/datasource';
 import { type MediaAspectRatioType, mediaAspectRatios } from '@/utilities/datasource';
+import { getProcessedImage } from '@/utilities/getProcessedImage';
+import { getSbImageSize } from '@/utilities/getSbImageSize';
 import * as styles from './EmbedMedia.styles';
 
 type EmbedMediaProps = React.HTMLAttributes<HTMLDivElement> & {
@@ -19,6 +21,7 @@ type EmbedMediaProps = React.HTMLAttributes<HTMLDivElement> & {
   spacingBottom?: PaddingType;
   isCaptionInset?: boolean;
   isPreview?: boolean;
+  previewImageSrc?: string;
   previewAriaLabel?: string;
 };
 
@@ -38,6 +41,7 @@ export const EmbedMedia = ({
   spacingBottom,
   isCaptionInset,
   isPreview,
+  previewImageSrc,
   previewAriaLabel,
   className,
   ...props
@@ -52,6 +56,45 @@ export const EmbedMedia = ({
       setHasWindow(true);
     }
   }, []);
+
+  let PreviewImage: ReactElement;
+  if (previewImageSrc) {
+    const { width: originalWidth, height: originalHeight } = getSbImageSize(previewImageSrc);
+    const cropHeight = Math.round(originalHeight * 1500 / originalWidth);
+
+    PreviewImage = (
+      <picture>
+        <source
+          srcSet={getProcessedImage(previewImageSrc, '1500x0')}
+          media="(min-width: 1200px)"
+        />
+        <source
+          srcSet={getProcessedImage(previewImageSrc, '1200x0')}
+          media="(min-width: 992px)"
+        />
+        <source
+          srcSet={getProcessedImage(previewImageSrc, '1000x0')}
+          media="(min-width: 768px)"
+        />
+        <source
+          srcSet={getProcessedImage(previewImageSrc, '800x0')}
+          media="(min-width: 576px)"
+        />
+        <source
+          srcSet={getProcessedImage(previewImageSrc, '600x0')}
+          media="(max-width: 575px)"
+        />
+        <img
+          src={getProcessedImage(previewImageSrc, '1500x0')}
+          loading="lazy"
+          width={1500}
+          height={cropHeight}
+          alt=""
+          className={styles.previewImage}
+        />
+      </picture>
+    );
+  }
 
   return (
     <WidthBox
@@ -72,7 +115,7 @@ export const EmbedMedia = ({
               url={mediaUrl}
               controls
               playsinline
-              light={isPreview ? true : false}
+              light={previewImageSrc && isPreview ? PreviewImage : isPreview}
               playing={isPreview ? true : false}
               playIcon={isPreview ? PlayPreviewIcon : undefined}
               // This previewAriaLabel prop is not documented but it is in the React Player source code
