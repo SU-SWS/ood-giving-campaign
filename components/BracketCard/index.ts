752d8163724e709b27f0d4cbbc5249a7ae1e4b90
Yvonne Tang <42749717+yvonnetangsu@users.noreply.github.com>
2025-02-18 17:27:20 -0800

    NOJIRA | Remove currently unused and POC components (#390)
    * Remove unused and POC components
    
    * Clean up logo files

--- components/BracketCard/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './BracketCard';
-export * from './BracketCard.styles';
--- components/SplitPoster/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './PosterContent';
-export * from './SplitPoster';
-export * from './SplitPoster.styles';
