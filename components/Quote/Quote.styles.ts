bad86325e35380b43cf77c4c68ec70ab3e654407
Yvonne Tang <42749717+yvonnetangsu@users.noreply.github.com>
2025-03-11 16:34:35 -0700

    DS-1147 | Quote alignment updates; metadata update to add dek as fallback for description (#397)
    * Quote alignment updates
    
    * Remove rich text conditionals for quote body because we decided on plain text
    
    * linter
    
    * Add fallback metadata description to dek
    
    * Use dek as fallback for og and twitter description too

--- components/Quote/Quote.styles.ts
+++ components/Quote/Quote.styles.ts
@@ -10,24 +10,23 @@ export type QuoteVerticalAlignType = keyof typeof verticalAlignments;
 export const root = (
   isMinimal: boolean,
   addDarkOverlay: boolean,
-  quoteOnRight: boolean,
+  barOnLeft: boolean,
   hasBar: boolean,
 ) => cnb('relative break-words flex flex-col lg:max-w-prose-wide lg:ml-0', {
   'max-w-[90%] sm:max-w-4/5 mx-auto md:max-w-full h-full': isMinimal && !addDarkOverlay,
   'bg-gc-black/50 backdrop-blur-sm h-full *:mb-0 text-white': addDarkOverlay && !isMinimal,
-  'rs-pl-4': !quoteOnRight && (!isMinimal || addDarkOverlay),
-  'rs-pr-4': quoteOnRight && (!isMinimal || addDarkOverlay),
+  'rs-pl-4': !barOnLeft && (!isMinimal || addDarkOverlay),
+  'rs-pr-4': barOnLeft && (!isMinimal || addDarkOverlay),
   'rs-px-4': addDarkOverlay && !hasBar,
 });
-export const content = (hasBar: boolean, quoteOnRight: boolean) => cnb({
-  'border-r-[1.4rem] 2xl:border-r-[2rem] rs-pr-2' : hasBar && !quoteOnRight,
-  'border-l-[1.4rem] 2xl:border-l-[2rem] rs-pl-2' : hasBar && quoteOnRight,
+export const content = (hasBar: boolean, barOnLeft: boolean) => cnb({
+  'border-r-[1.4rem] 2xl:border-r-[2rem] pr-20 md:pr-36 2xl:pr-38' : hasBar && !barOnLeft,
+  'border-l-[1.4rem] 2xl:border-l-[2rem] pl-20 md:pl-36 2xl:pl-38' : hasBar && barOnLeft,
 });
-export const teaserWrapper = 'w-fit gap-9';
-export const quoteMark = (isLargeTeaser: boolean) => cnb('shrink-0 leading-[0]', {
+export const quoteMark = (isLargeTeaser: boolean) => cnb('leading-[0]', {
   'text-[clamp(16rem,7.46vw+13.31rem,26rem)] mt-[clamp(5.7rem,2.61vw+4.76rem,9.2rem)]': !isLargeTeaser,
   'text-[clamp(17rem,11.19vw+12.97rem,32rem)] mt-[clamp(6rem,4.03vw+4.55rem,11.4rem)]': isLargeTeaser,
 });
 export const teaser = 'rs-mb-0 grow-0 w-fit';
-export const body = 'mt-02em first:mt-0 whitespace-pre-line';
+export const body = (hasTeaser: boolean) => cnb('whitespace-pre-line', hasTeaser && 'mt-02em');
 export const source = 'rs-mt-1 whitespace-pre-line';
