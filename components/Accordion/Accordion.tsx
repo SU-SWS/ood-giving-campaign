1053bd45321cbd5ad237cbb392d4f1ddb87e7fc4
Yvonne Tang <ytang2@stanford.edu>
2025-03-18 13:04:15 -0700

    no focus within accordion content if not expanded;  hocus styles

--- components/Accordion/Accordion.tsx
+++ components/Accordion/Accordion.tsx
@@ -84,8 +84,8 @@ export const Accordion = ({
       )}
       <ul className="list-unstyled rs-mt-2 max-w-1000">
         {items?.map((item, index) => (
-          <li key={item._uid} className="mb-0 border-b first:border-t border-black-60 pt-16 pb-18 pl-10 md:px-20">
-            <Heading variant="big" as={item.headingLevel} color={isDarkTheme ? 'white' : 'black'} leading="tight" className="w-full mb-0">
+          <li key={item._uid} className="relative mb-0 border-b first:border-t border-black-60">
+            <Heading variant="big" as={item.headingLevel} color={isDarkTheme ? 'white' : 'black'} leading="tight" className="relative w-full mb-0">
               <CtaButton
                 id={`heading-${item._uid}`}
                 onClick={() => toggleItem(index)}
@@ -93,11 +93,11 @@ export const Accordion = ({
                 color={isDarkTheme ? 'white' : 'black'}
                 aria-expanded={openItems[index] || false}
                 aria-controls={`content-${item._uid}`}
-                className="relative w-full text-left pr-40"
+                className="group relative w-full text-left pr-36 md:pr-50 pt-16 pb-18 pl-12 md:pl-22"
               >
-                <span aria-hidden="true" className="absolute top-0 left-0 w-5 bottom-0 bg-digital-red-light" />
+                <span aria-hidden="true" className="absolute top-0 left-0 group-hocus-visible:w-6 group-hocus-visible:md:w-8 scale-y-0 transition-transform group-hocus-visible:scale-y-100 bottom-0 bg-digital-red-light" />
                 {item.heading}
-                <HeroIcon icon={openItems[index] ? 'minus' : 'plus'} className="shrink-0 grow-0 absolute right-0 w-30 h-30 border-black-60 border-2 p-3 rounded-full" />
+                <HeroIcon icon={openItems[index] ? 'minus' : 'plus'} className="group-hocus-visible:rotate-180 shrink-0 grow-0 absolute right-0 md:right-20 w-24 h-24 md:w-30 md:h-30 border-black-60 border-2 p-2 md:p-3 rounded-full" />
               </CtaButton>
             </Heading>
             <m.div
@@ -107,13 +107,13 @@ export const Accordion = ({
               aria-hidden={!openItems[index]}
               animate={{
                 height: openItems[index] ? 'auto' : 0,
-                opacity: openItems[index] ? 1 : 0.4,
+                visibility: openItems[index] ? 'visible' : 'hidden',
               }}
               initial={false}
               transition={{ duration: 0.3, ease: 'easeIn' }}
               className="overflow-clip"
             >
-              <div className="rs-pt-2">
+              <div className="rs-pt-0 rs-pb-2 pl-10 md:px-20">
                 {hasRichText(item.content) && (
                   <RichText
                     type="card"
