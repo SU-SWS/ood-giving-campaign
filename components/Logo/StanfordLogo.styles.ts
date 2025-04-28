9c63c654edd888e454db8daf60a9a0649d386d54
Yvonne Tang <ytang2@stanford.edu>
2025-04-28 13:20:12 -0700

    Remove digital red logo option and change global footer to black

--- components/Logo/StanfordLogo.styles.ts
+++ components/Logo/StanfordLogo.styles.ts
@@ -1,6 +1,5 @@
 export const stanfordLogoColors = {
   'cardinal-red': 'text-cardinal-red hocus:text-cardinal-red',
-  'digital-red': 'text-digital-red hocus:text-digital-red',
   black: 'text-gc-black hocus:text-gc-black',
   white: 'text-white hocus:text-white',
 };
