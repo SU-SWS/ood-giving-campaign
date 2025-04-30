97ba496df86d0525bbbfe3a2c59750e08ecde3cf
Yvonne Tang <42749717+yvonnetangsu@users.noreply.github.com>
2025-04-29 17:59:07 -0700

    A11y | DS-1510 | Add role="button" to clickable video preview wrapper (#411)
    * Add role="button" to clickable preview
    
    * Update getNumBloks
    
    * If the space bar is hit, don't scroll the page just play the video
    
    * Add comment back

--- components/EmbedMedia/EmbedMedia.tsx
+++ components/EmbedMedia/EmbedMedia.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { cnb } from 'cnbuilder';
 import ReactPlayer from 'react-player/lazy';
+import { useEventListener } from 'usehooks-ts';
 import { Caption, type CaptionProps } from '@/components/Media/Caption';
 import { FlexBox } from '@/components/FlexBox';
 import { PreviewImage } from './PreviewImage';
@@ -49,13 +50,48 @@ export const EmbedMedia = ({
    * https://github.com/cookpete/react-player/issues/1428
    */
   const [hasWindow, setHasWindow] = useState(false);
+  const playerWrapperRef = useRef<HTMLDivElement>(null);
+  const PreviewImg = previewImageSrc ? <PreviewImage previewImageSrc={previewImageSrc} /> : null;
+
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setHasWindow(true);
-    }
+    setHasWindow(typeof window !== 'undefined');
   }, []);
 
-  const PreviewImg = previewImageSrc ? <PreviewImage previewImageSrc={previewImageSrc} /> : null;
+  // If the space key is pressed, do not scroll the page and click the element
+  const handleSpaceKeyDown = (event: KeyboardEvent) => {
+    if (event.code === 'Space' || event.key === ' ') {
+      event.preventDefault();
+      (event.target as HTMLElement).click();
+    }
+  };
+
+  useEffect(() => {
+    if (!isPreview || !playerWrapperRef.current) return;
+
+    const wrapper = playerWrapperRef.current;
+
+    /**
+     * Observe for an element with class .react-player__preview inside the wrapper
+     * and set its role to button when it is added to the DOM
+     */
+    const observer = new MutationObserver(() => {
+      const preview = wrapper.querySelector('.react-player__preview');
+      if (preview) {
+        preview.setAttribute('role', 'button');
+        observer.disconnect();
+      }
+    });
+
+    observer.observe(wrapper, {
+      childList: true, // Look for added/removed child elements
+      subtree: true, // Look for added/removed elements in all descendants inside wrapper
+    });
+
+    // Cleanup on unmount
+    return () => observer.disconnect();
+  }, [isPreview]);
+
+  useEventListener('keydown', handleSpaceKeyDown, playerWrapperRef);
 
   return (
     <WidthBox
@@ -68,7 +104,7 @@ export const EmbedMedia = ({
     >
       <figure>
         {/* Extra classnames passed into wrapper for Vimeo responsive bug */}
-        <div className={cnb(mediaAspectRatios[aspectRatio], styles.mediaWrapper)}>
+        <div className={cnb(mediaAspectRatios[aspectRatio], styles.mediaWrapper)} ref={playerWrapperRef}>
           {hasWindow && (
             <ReactPlayer
               width="100%"
