2abc4c3740ad44c3409de115c2a247e970fa856b
Yvonne Tang <ytang2@stanford.edu>
2025-09-04 12:00:58 -0700

    Use decanter v8 branch and update for tw4; start adding some momentum themes

--- tailwind/plugins/theme/gc-lineHeight.js
+++
@@ -1,7 +0,0 @@
-/**
- * Momentum line heights
- */
-export const gcLineHeight = () => ({
-  // Extra tight line height for Druk font in some components, e.g., Data Card, Moment Poster
-  druk: '0.9',
-});
--- tailwind/plugins/theme/gc-screens.js
+++
@@ -1,8 +0,0 @@
-/**
- * 2 extra breakpoints + print support
- * 3xl: 2xl + 2 x the screen edge margins
- * 4xl: 2000px
- */
-export const gcScreens = () => ({
-  'tf-sm': '658px',
-});
