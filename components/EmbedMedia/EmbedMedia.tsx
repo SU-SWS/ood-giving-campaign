c9bf1d826b51865bf226a474463a15c4a0411fd2
Yvonne Tang <ytang2@stanford.edu>
2026-01-29 17:17:49 -0800

    Update EmbedMedia window check

--- components/EmbedMedia/EmbedMedia.tsx
+++ components/EmbedMedia/EmbedMedia.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import { cnb } from 'cnbuilder';
 import ReactPlayer from 'react-player/lazy';
 import { useEventListener } from 'usehooks-ts';
@@ -47,17 +47,8 @@ export const EmbedMedia = ({
   className,
   ...props
 }: EmbedMediaProps) => {
-  /**
-   * This is needed to prevent hydration error for the React Player.
-   * https://github.com/cookpete/react-player/issues/1428
-   */
-  const [hasWindow, setHasWindow] = useState(false);
   const playerWrapperRef = useRef<HTMLDivElement>(null);
   const PreviewImg = previewImageSrc ? <PreviewImage previewImageSrc={previewImageSrc} /> : null;
-
-  useEffect(() => {
-    setHasWindow(typeof window !== 'undefined');
-  }, []);
 
   // If the space key is pressed, do not scroll the page and click the element
   const handleSpaceKeyDown = (event: KeyboardEvent) => {
@@ -107,21 +98,19 @@ export const EmbedMedia = ({
       <figure>
         {/* Extra classnames passed into wrapper for Vimeo responsive bug */}
         <div className={cnb(mediaAspectRatios[aspectRatio], styles.mediaWrapper)} ref={playerWrapperRef}>
-          {hasWindow && (
-            <ReactPlayer
-              width="100%"
-              height="100%"
-              url={mediaUrl}
-              controls
-              playsinline
-              light={previewImageSrc && isPreview ? PreviewImg : isPreview}
-              playing={isPreview}
-              playIcon={isPreview ? PlayPreviewIcon : undefined}
-              // This previewAriaLabel prop is not documented but it is in the React Player source code
-              previewAriaLabel={isPreview ? previewAriaLabel : undefined}
-              className="group"
-            />
-          )}
+          <ReactPlayer
+            width="100%"
+            height="100%"
+            url={mediaUrl}
+            controls
+            playsinline
+            light={previewImageSrc && isPreview ? PreviewImg : isPreview}
+            playing={isPreview}
+            playIcon={isPreview ? PlayPreviewIcon : undefined}
+            // This previewAriaLabel prop is not documented but it is in the React Player source code
+            previewAriaLabel={isPreview ? previewAriaLabel : undefined}
+            className="group"
+          />
         </div>
         {caption && (
           <Caption caption={caption} isCaptionInset={isCaptionInset} captionBgColor={captionBgColor} />
